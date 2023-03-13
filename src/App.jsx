25a013c2540542c4f8a8f5eebcbba3bdfe03f132
gift56 <efeasiughu@gmail.com>
2023-03-13 10:42:35 -0700

    working on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default App;
