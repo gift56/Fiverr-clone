b64e9f062eeb718ef16c66dba38770295f138771
gift56 <efeasiughu@gmail.com>
2023-03-13 12:44:19 -0700

    adding stylings

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { TfiWorld } from "react-icons/tfi";
 import { BsCurrencyDollar } from "react-icons/bs";
 import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   const [active, setActive] = useState(false);
+  useEffect(() => {
+    const backgroundChange = () => {
+      window.scrollY > 0 ? setActive(true) : setActive(false);
+    };
+    window.addEventListener("scroll", backgroundChange);
+    return () => {
+      window.removeEventListener("scroll", backgroundChange);
+    };
+  }, []);
+
+  const currentUser = {
+    id: 1,
+    username: "Efe Gift",
+    isSeller: true,
+  };
+
   return (
     <header
-      className={`flex items-center justify-center w-full flex-col text-white ${
-        active ? "bg-white text-black" : "bg-navbg"
+      className={`flex items-center justify-center w-full flex-col text-white sticky top-0 transition-all ease-in-out z-10 ${
+        active ? "bg-white !text-black" : "bg-navbg"
       }`}
     >
       <div className="contain">
@@ -36,9 +52,11 @@ const Navbar = () => {
               </span>
               USD
             </span>
-            <NavLink to="seller" className="cursor-pointer">
-              Become a Seller
-            </NavLink>
+            {!currentUser.isSeller && (
+              <NavLink to="seller" className="cursor-pointer">
+                Become a Seller
+              </NavLink>
+            )}
             <NavLink to="/login" className="cursor-pointer">
               Sign in
             </NavLink>
@@ -56,41 +74,41 @@ const Navbar = () => {
       </div>
       <div
         className={`w-full transition-all duration-300 ${
-          active ? "block" : "hidden"
+          active ? "flex" : "hidden"
         }`}
       >
-        <hr className="border-gray-300" />
+        <hr className="border-black" />
         <div className="contain">
           <div
-            className={`w-full flex items-center justify-between py-3 overflow-x-auto gap-5 font-medium text-gray-200 ${
+            className={`w-full flex items-center justify-between py-3 overflow-x-auto gap-5 font-medium text-gray-200 scrollbar-hide text-sm ${
               active ? "text-gray-500" : ""
             }`}
           >
-            <span className="hover:border-b-2 cursor-pointer transition-[border]  h-8 border-primary">
+            <span className="hover:border-b-2 cursor-pointer transition-[border] h-8 scrollbar-hide border-primary">
               Graphics & Design
             </span>
-            <span className="hover:border-b-2 cursor-pointer transition-[border] duration-100 h-8 border-primary">
+            <span className="hover:border-b-2 cursor-pointer transition-[border] duration-100 h-8 scrollbar-hide border-primary">
               Digital Marketing
             </span>
-            <span className="hover:border-b-2 cursor-pointer transition-[border] duration-100 h-8 border-primary">
+            <span className="hover:border-b-2 cursor-pointer transition-[border] duration-100 h-8 scrollbar-hide border-primary">
               Writing & Translation
             </span>
-            <span className="hover:border-b-2 cursor-pointer transition-[border] duration-100 h-8 border-primary">
+            <span className="hover:border-b-2 cursor-pointer transition-[border] duration-100 h-8 scrollbar-hide border-primary">
               Video & Animation
             </span>
-            <span className="hover:border-b-2 cursor-pointer transition-[border] duration-100 h-8 border-primary">
+            <span className="hover:border-b-2 cursor-pointer transition-[border] duration-100 h-8 scrollbar-hide border-primary">
               Music & Audio
             </span>
-            <span className="hover:border-b-2 cursor-pointer transition-[border] duration-100 h-8 border-primary">
+            <span className="hover:border-b-2 cursor-pointer transition-[border] duration-100 h-8 scrollbar-hide border-primary">
               Programming & Tech
             </span>
-            <span className="hover:border-b-2 cursor-pointer transition-[border] duration-100 h-8 border-primary">
+            <span className="hover:border-b-2 cursor-pointer transition-[border] duration-100 h-8 scrollbar-hide border-primary">
               Business
             </span>
-            <span className="hover:border-b-2 cursor-pointer transition-[border] duration-100 h-8 border-primary">
+            <span className="hover:border-b-2 cursor-pointer transition-[border] duration-100 h-8 scrollbar-hide border-primary">
               Lifestyle
             </span>
-            <span className="hover:border-b-2 cursor-pointer transition-[border] duration-100 h-8 border-primary">
+            <span className="hover:border-b-2 cursor-pointer transition-[border] duration-100 h-8 scrollbar-hide border-primary">
               AI Services
             </span>
           </div>
