d838c0ed435067ceb7d5825ab0c277df8e06106e
gift56 <efeasiughu@gmail.com>
2023-03-13 15:41:07 -0700

    working on client page

--- src/components/HomeComponents/Clients/Clients.jsx
+++ src/components/HomeComponents/Clients/Clients.jsx
@@ -1,8 +1,27 @@
 import React from "react";
-import { BsMeta } from "react-icons/bs";
+import client1 from "../../../assets/icons/client1.png";
+import client2 from "../../../assets/icons/client2.png";
+import client3 from "../../../assets/icons/client3.png";
+import client4 from "../../../assets/icons/client4.png";
+import client5 from "../../../assets/icons/client5.png";
 
 const Clients = () => {
-  return <div className="">Clients</div>;
+  const clientsData = [client1, client2, client3, client4, client5];
+
+  return (
+    <section className="w-full bg-clientBg py-6">
+      <div className="contain">
+        <div className="flex items-center justify-center w-full gap-4">
+          <h2>Trusted by:</h2>
+          <div className="flex items-center justify-start gap-4">
+            {clientsData.map((item, i) => (
+              <img src={item} alt={i} key={i} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Clients;
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Clients from "../../components/HomeComponents/Clients/Clients";
 import Hero from "../../components/HomeComponents/Hero/Hero";
 
 const Homepage = () => {
   return (
     <main>
       <Hero />
+      <Clients />
     </main>
   );
 };
