e91bc8ad32fcdb7162e06f1a36e41fbc1394d649
gift56 <efeasiughu@gmail.com>
2023-03-13 12:08:19 -0700

    adding stylings

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { TfiWorld } from "react-icons/tfi";
 import { BsCurrencyDollar } from "react-icons/bs";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <header className="flex items-center justify-center bg-navbg text-white">
+    <header className="flex items-center justify-center w-full flex-col bg-navbg text-white">
       <div className="contain">
         <div className="w-full flex items-center justify-between py-5">
           <Link to="/" className="text-3xl font-bold">
@@ -13,29 +13,72 @@ const Navbar = () => {
             <span className="text-primary">.</span>
           </Link>
           <nav className="flex items-center justify-end gap-7 font-medium text-base">
-            <span className="cursor-pointer">Fiverr Business</span>
-            <span className="cursor-pointer">Explore</span>
-            <span className="flex items-center gap-2 cursor-pointer">
+            <NavLink to="/" className="cursor-pointer">
+              Fiverr Business
+            </NavLink>
+            <NavLink to="/explore" className="cursor-pointer">
+              Explore
+            </NavLink>
+            <div className="flex items-center gap-2 cursor-pointer">
               <span>
                 <TfiWorld />
               </span>
               English
-            </span>
+            </div>
             <span className="flex items-center gap-2 cursor-pointer">
               <span>
                 <BsCurrencyDollar />
               </span>
               USD
             </span>
-            <span className="cursor-pointer">Become a Seller</span>
-            <span className="cursor-pointer">Sign in</span>
-            <button
+            <NavLink to="seller" className="cursor-pointer">
+              Become a Seller
+            </NavLink>
+            <NavLink to="/login" className="cursor-pointer">
+              Sign in
+            </NavLink>
+            <NavLink
+              to="/join"
               type="button"
-              className="border outline-none py-1 px-5 rounded hover:bg-primary hover:text-white transition-all duration-300 text-sm font-medium"
+              className="border py-2 px-5 rounded hover:bg-primary hover:border-primary hover:text-white transition-all duration-300 text-sm font-medium"
             >
               Join
-            </button>
+            </NavLink>
           </nav>
+        </div>
+      </div>
+      <div className="w-full">
+        <hr className="border-gray-300" />
+        <div className="contain">
+          <div className="w-full flex items-center justify-between py-3 overflow-x-auto gap-5 font-light text-gray-200">
+            <span className="hover:border-b-2 cursor-pointer transition-[border]  h-8 border-primary">
+              Graphics & Design
+            </span>
+            <span className="hover:border-b-2 cursor-pointer transition-[border] duration-100 h-8 border-primary">
+              Digital Marketing
+            </span>
+            <span className="hover:border-b-2 cursor-pointer transition-[border] duration-100 h-8 border-primary">
+              Writing & Translation
+            </span>
+            <span className="hover:border-b-2 cursor-pointer transition-[border] duration-100 h-8 border-primary">
+              Video & Animation
+            </span>
+            <span className="hover:border-b-2 cursor-pointer transition-[border] duration-100 h-8 border-primary">
+              Music & Audio
+            </span>
+            <span className="hover:border-b-2 cursor-pointer transition-[border] duration-100 h-8 border-primary">
+              Programming & Tech
+            </span>
+            <span className="hover:border-b-2 cursor-pointer transition-[border] duration-100 h-8 border-primary">
+              Business
+            </span>
+            <span className="hover:border-b-2 cursor-pointer transition-[border] duration-100 h-8 border-primary">
+              Lifestyle
+            </span>
+            <span className="hover:border-b-2 cursor-pointer transition-[border] duration-100 h-8 border-primary">
+              AI Services
+            </span>
+          </div>
         </div>
       </div>
     </header>
