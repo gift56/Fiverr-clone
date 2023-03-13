e0ab38875b74966f707a436788b88319f5933e9f
gift56 <efeasiughu@gmail.com>
2023-03-13 11:05:35 -0700

    working on navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { TfiWorld } from "react-icons/tfi";
+import { BsCurrencyDollar } from "react-icons/bs";
 
 const Navbar = () => {
-  return <header>Navbar</header>;
+  return (
+    <header>
+      <div className="contain">
+        <div>
+          <div className="logo">
+            <span>Fiverr</span>
+            <span>.</span>
+          </div>
+          <div className="links">
+            <span>Fiverr Business</span>
+            <span>Explore</span>
+            <span>
+              <span>
+                <TfiWorld />
+              </span>
+              English
+            </span>
+            <span>
+              <span>
+                <BsCurrencyDollar />
+              </span>
+              USD
+            </span>
+            <span>Become a Seller</span>
+            <span>Sign In</span>
+            <button>Join</button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
