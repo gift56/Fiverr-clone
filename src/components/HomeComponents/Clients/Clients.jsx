5fe45075de9bfda1d8a3d287a6843aa80cff0177
gift56 <efeasiughu@gmail.com>
2023-03-13 15:35:17 -0700

    working on client page

--- src/components/HomeComponents/Clients/Clients.jsx
+++ src/components/HomeComponents/Clients/Clients.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BsMeta } from "react-icons/bs";
 
 const Clients = () => {
-  return <div>Clients</div>;
+  return <div className="">Clients</div>;
 };
 
 export default Clients;
