5cf7063fdb98ef1596e44ecb0d5a64209494e552
gift56 <efeasiughu@gmail.com>
2023-03-13 15:18:26 -0700

    done with hero page

--- src/components/HomeComponents/Hero/Hero.jsx
+++ src/components/HomeComponents/Hero/Hero.jsx
@@ -1,11 +1,28 @@
 import React from "react";
+import { FiSearch } from "react-icons/fi";
 
 const Hero = () => {
   return (
     <section className="w-full bg-navbg h-screen text-white animate-bganimate bg-no-repeat bg-cover bg-top-4 pt-20">
       <div className="contain flex flex-col items-start justify-center h-full">
         <div className="flex flex-col items-start justify-start gap-4">
-          <h2>Find the perfect freelance services for your business</h2>
+          <h2 className="text-5xl font-semibold leading-[120%]">
+            Find the perfect <i className="font-light">freelance</i> <br />{" "}
+            services for your business
+          </h2>
+          <div className="flex items-center w-[650px] bg-white h-[50px] rounded-md focus:border focus:border-primary">
+            <div className="flex items-center justify-start gap-2 w-full h-full">
+              <span className="text-active pl-3">
+                <FiSearch size={18} />
+              </span>
+              <input
+                type="text"
+                placeholder='Try "building mobile app"'
+                className="w-full h-full bg-transparent outline-none text-active"
+              />
+            </div>
+            <button type="submit" className="bg-primary h-full w-[150px] rounded-tr-md rounded-br-md">Search</button>
+          </div>
         </div>
       </div>
     </section>
