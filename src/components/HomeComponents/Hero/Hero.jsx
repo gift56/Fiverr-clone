cf6cdd1d61740af27187e1867e8b99308d38ea2c
gift56 <efeasiughu@gmail.com>
2023-03-13 13:59:23 -0700

    working on hero page

---
+++ src/components/HomeComponents/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section>
+        <div className="contain">
+            <div></div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Hero from "../../components/HomeComponents/Hero/Hero";
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
-  )
-}
+    <main>
+      <Hero />
+    </main>
+  );
+};
 
-export default Homepage
+export default Homepage;
