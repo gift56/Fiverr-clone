8bf5561289b4cf5cd1f38b2a0b37a5755ef652b5
gift56 <efeasiughu@gmail.com>
2023-03-13 11:11:29 -0700

    adding stylings

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1dbf73",
+        darkColor: "#1e1692",
+      },
+    },
   },
   plugins: [],
 };
