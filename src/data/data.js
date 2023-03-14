ff895b73af579557b6d94f22ede3755ba2915020
gift56 <efeasiughu@gmail.com>
2023-03-14 08:37:23 -0700

    creating fake data

--- src/data/data.js
+++ src/data/data.js
@@ -12,5 +12,62 @@ import image10 from "../assets/images/slide10.png";
 export const cards = [
   {
     id: "001",
+    title: "AI Artists",
+    description: "Add talent to AI",
+    img: image1,
+  },
+  {
+    id: "002",
+    title: "Logo Design",
+    description: "Build your brand",
+    img: image2,
+  },
+  {
+    id: "003",
+    title: "WordPress",
+    description: "Customize your site",
+    img: image3,
+  },
+  {
+    id: "004",
+    title: "Voice Over",
+    description: "Share your message",
+    img: image4,
+  },
+  {
+    id: "005",
+    title: "Video Explainer",
+    description: "Engage your audience",
+    img: image5,
+  },
+  {
+    id: "006",
+    title: "Social Media",
+    description: "Reach more customers",
+    img: image6,
+  },
+  {
+    id: "007",
+    title: "SEO",
+    description: "Unlock growth online",
+    img: image7,
+  },
+  {
+    id: "008",
+    title: "Illustration",
+    description: "Color your dreams",
+    img: image8,
+  },
+  {
+    id: "009",
+    title: "Translation",
+    description: "Go global",
+    img: image9,
+  },
+  {
+    id: "0010",
+    title: "Data Entry",
+    description: "Learn your business",
+    img: image10,
   },
 ];
