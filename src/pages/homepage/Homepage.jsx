e1e98014694c947c5790de21705fcebfb05143f5
gift56 <efeasiughu@gmail.com>
2023-03-14 07:07:34 -0700

    gotten slider images

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Clients from "../../components/HomeComponents/Clients/Clients";
 import Hero from "../../components/HomeComponents/Hero/Hero";
+import Services from "../../components/HomeComponents/Services/Services";
 
 const Homepage = () => {
   return (
     <main>
       <Hero />
       <Clients />
+      <Services />
     </main>
   );
 };
