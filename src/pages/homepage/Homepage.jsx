c70f44a479e09bdd4237a8236a54027b413dcb22
gift56 <efeasiughu@gmail.com>
2023-03-14 14:40:36 -0700

    working on testimony page

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -4,10 +4,11 @@ import Hero from "../../components/HomeComponents/Hero/Hero";
 import Slides from "../../components/Slides/Slides";
 import SingleCard from "../../components/HomeComponents/Services/SingleCard/SingleCard";
 import Features from "../../components/HomeComponents/Features/Features";
-import { cards, testimonyData } from "../../data/data";
+import { cards, projectsCards, testimonyData } from "../../data/data";
 import Marketplace from "../../components/HomeComponents/Marketplace/Marketplace";
 import Business from "../../components/HomeComponents/Business/Business";
 import Testimony from "../../components/HomeComponents/Testimony/Testimony";
+import Projects from "../../components/HomeComponents/Projects/Projects";
 
 const Homepage = () => {
   return (
@@ -25,6 +26,11 @@ const Homepage = () => {
       <Slides rowId="2" distance={1200}>
         {testimonyData.map((item, i) => (
           <Testimony item={item} key={i} />
+        ))}
+      </Slides>
+      <Slides rowId="3" distance={500}>
+        {projectsCards.map((item, i) => (
+          <Projects item={item} key={i} />
         ))}
       </Slides>
     </main>
