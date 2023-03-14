bae9143a7fc58031d16d4e39bb650b623b568e90
gift56 <efeasiughu@gmail.com>
2023-03-14 09:59:21 -0700

    adding some features

--- src/components/HomeComponents/Features/Features.jsx
+++ src/components/HomeComponents/Features/Features.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { BsCheckCircle } from "react-icons/bs";
 
 const Features = () => {
   return (
-    <section>
+    <section className="bg-[#f1fdf7] py-10">
       <div className="contain">Features</div>
     </section>
   );
