641a7b295f0af889f654097dbf675e8a8064a89d
gift56 <efeasiughu@gmail.com>
2023-03-14 13:27:49 -0700

    working on testimony page

--- src/components/HomeComponents/Testimony/Testimony.jsx
+++ src/components/HomeComponents/Testimony/Testimony.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import userImg from '../../../assets/images/foundedImg.png'
 
 const Testimony = () => {
   return (
-    <div className="relative inline-block cursor-pointer w-full mx-4">Hello</div>
-  )
-}
+    <div className="relative inline-block cursor-pointer w-full mx-4">
+      Hello
+    </div>
+  );
+};
 
-export default Testimony
+export default Testimony;
