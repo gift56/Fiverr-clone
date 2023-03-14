8842c50a516e0f493acda6ad78e5af86d5fa32ff
gift56 <efeasiughu@gmail.com>
2023-03-14 12:38:21 -0700

    working on business page

--- src/components/HomeComponents/Business/Business.jsx
+++ src/components/HomeComponents/Business/Business.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
+import { BsCheckCircle } from "react-icons/bs";
+import userImg from "../../../assets/images/";
 
 const Business = () => {
-  return (
-    <section className='py-14 bg-darkblue text-white'>Business</section>
-  )
-}
+  return <section className="py-14 bg-darkblue text-white">Business</section>;
+};
 
-export default Business
+export default Business;
