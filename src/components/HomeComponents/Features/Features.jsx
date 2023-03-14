39fc5c47d4dbdb8be446edcfaba771b3709c21fc
gift56 <efeasiughu@gmail.com>
2023-03-14 10:11:36 -0700

    added video

--- src/components/HomeComponents/Features/Features.jsx
+++ src/components/HomeComponents/Features/Features.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { BsCheckCircle } from "react-icons/bs";
 
-
 const Features = () => {
   return (
     <section className="bg-[#f1fdf7] py-10">
       <div className="contain">
-        <div className="flex items-center justify-between w-full"></div>
+        <div className="flex items-center justify-between w-full">
+          <div className="flex-1"></div>
+          <div className="flex-1">
+            <video src="https://fiverr-res.cloudinary.com/video/upload/t_fiverr_hd/vmvv3czyk2ifedefkau7" controls></video>
+          </div>
+        </div>
       </div>
     </section>
   );
