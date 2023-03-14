8949001f0ff2bab6fb1de3eb718ef1bcc73c26b2
gift56 <efeasiughu@gmail.com>
2023-03-14 09:25:34 -0700

    positioning text

--- src/components/HomeComponents/Services/SingleCard/SingleCard.jsx
+++ src/components/HomeComponents/Services/SingleCard/SingleCard.jsx
@@ -5,9 +5,13 @@ const SingleCard = ({ item }) => {
   return (
     <Link
       to={`/gigs?cat=design`}
-      className="relative p-2 inline-block cursor-pointer w-[160px] sm:w-[200px] md:w-[240px] lg:w-[250px] mx-4"
+      className="relative inline-block cursor-pointer w-[160px] sm:w-[200px] md:w-[240px] lg:w-[250px] mx-4"
     >
       <img src={item.img} alt={item.title} className="w-full h-auto block" />
+      <div className="absolute top-9 text-white left-4 text-base font-">
+        <h2>{item.title}</h2>
+        <p>{item?.description}</p>
+      </div>
     </Link>
   );
 };
