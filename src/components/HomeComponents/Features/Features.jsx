08afa7d3fc1822551d39fb9130428b88796b1d15
gift56 <efeasiughu@gmail.com>
2023-03-14 10:16:40 -0700

    added new images

--- src/components/HomeComponents/Features/Features.jsx
+++ src/components/HomeComponents/Features/Features.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import { useState, useRef, useEffect } from "react";
 import { BsCheckCircle } from "react-icons/bs";
 
 const Features = () => {
+  const [showVideo, setShowVideo] = useState(false);
+  const videoRef = useRef(null);
+  
   return (
     <section className="bg-[#f1fdf7] py-10">
       <div className="contain">
         <div className="flex items-center justify-between w-full">
           <div className="flex-1"></div>
           <div className="flex-1">
-            <video src="https://fiverr-res.cloudinary.com/video/upload/t_fiverr_hd/vmvv3czyk2ifedefkau7" controls></video>
+            <button>Click me</button>
+            <video
+              className="rounded-md"
+              src="https://fiverr-res.cloudinary.com/video/upload/t_fiverr_hd/vmvv3czyk2ifedefkau7"
+              controls
+            ></video>
           </div>
         </div>
       </div>
