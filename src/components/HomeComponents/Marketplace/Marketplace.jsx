3876c5eb20cf3643d2616f4485fb2618a45e603d
gift56 <efeasiughu@gmail.com>
2023-03-14 12:21:13 -0700

    working on marketplace

--- src/components/HomeComponents/Marketplace/Marketplace.jsx
+++ src/components/HomeComponents/Marketplace/Marketplace.jsx
@@ -6,10 +6,24 @@ const Marketplace = () => {
   return (
     <section className="py-14 bg-white">
       <div className="contain">
-        <div className="flex items-center justify-between w-full gap-4">
+        <div className="flex flex-col items-start justify-between w-full gap-9">
           <h2 className="text-4xl font-bold text-darkColor">
             Explore the marketplace
           </h2>
+          <div className="w-full grid grid-cols-5 justify-center items-center gap-7 mt-6">
+            {marketsData.map((item, i) => (
+              <Link
+                to="/"
+                key={i}
+                className="w-full flex items-center justify-center flex-col gap-4"
+              >
+                <div className="w-12 h-12 relative">
+                  <img src={item.icon} alt="" className="w-full" />
+                </div>
+                <h2 className="text-sm text-center font-normal">{item.title}</h2>
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </section>
