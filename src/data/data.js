615428d01e4ad017433b4598c72404d54de363b9
gift56 <efeasiughu@gmail.com>
2023-03-14 08:17:32 -0700

    creating fake data

--- src/data/data.js
+++ src/data/data.js
@@ -1,4 +1,13 @@
 import image1 from "../assets/images/slide1.png";
+import image2 from "../assets/images/slide2.png";
+import image3 from "../assets/images/slide3.png";
+import image4 from "../assets/images/slide4.png";
+import image5 from "../assets/images/slide5.png";
+import image6 from "../assets/images/slide6.png";
+import image7 from "../assets/images/slide7.png";
+import image8 from "../assets/images/slide8.png";
+import image9 from "../assets/images/slide9.png";
+import image10 from "../assets/images/slide10.png";
 
 export const cards = [
   {
