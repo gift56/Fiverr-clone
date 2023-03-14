b9d6aee8e6f60182d757fb9b1153331ad2c992b6
gift56 <efeasiughu@gmail.com>
2023-03-14 15:06:37 -0700

    done working on projects section

--- src/components/HomeComponents/Projects/Projects.jsx
+++ src/components/HomeComponents/Projects/Projects.jsx
@@ -8,9 +8,23 @@ const Projects = ({ item }) => {
       className="relative inline-block cursor-pointer w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] mx-4 shadow-box"
     >
       <div className="w-full flex items-start flex-col justify-start">
-        <img src={item.img} alt={item.description} className="w-full h-[300px] rounded-tr-md rounded-tl-md object-cover" />
-        <div className="w-full flex items-start px-4 rounded-br-md rounded-bl-md bg-white py-4">
-
+        <img
+          src={item.img}
+          alt={item.description}
+          className="w-full h-[300px] rounded-tr-md rounded-tl-md object-cover"
+        />
+        <div className="w-full flex items-center justify-start gap-2 px-4 rounded-br-md rounded-bl-md bg-white py-4 border">
+          <div className="w-10 h-10">
+            <img
+              src={item.userImg}
+              alt={item.description}
+              className="w-full object-cover rounded-full"
+            />
+          </div>
+          <div>
+            <h2 className="text-darkColor font-semibold text-sm">{item.title}</h2>
+            <p className="text-gray-400 text-sm font-normal">{item.description}</p>
+          </div>
         </div>
       </div>
     </Link>
