52fd9a289b1962ae58d0a605f24caa61a551d454
gift56 <efeasiughu@gmail.com>
2023-03-14 09:30:11 -0700

    done with service page

--- src/components/HomeComponents/Services/SingleCard/SingleCard.jsx
+++ src/components/HomeComponents/Services/SingleCard/SingleCard.jsx
@@ -8,9 +8,9 @@ const SingleCard = ({ item }) => {
       className="relative inline-block cursor-pointer w-[160px] sm:w-[200px] md:w-[240px] lg:w-[250px] mx-4"
     >
       <img src={item.img} alt={item.title} className="w-full h-auto block" />
-      <div className="absolute top-9 text-white left-4 text-base font-">
-        <h2>{item.title}</h2>
-        <p>{item?.description}</p>
+      <div className="absolute top-4 text-white left-4">
+        <p className="text-sm font-normal">{item?.description}</p>
+        <h2 className="text-2xl font-bold tracking-wide">{item.title}</h2>
       </div>
     </Link>
   );
