8254dcb6973c4bdda8108e96eef221bb9d7fd193
gift56 <efeasiughu@gmail.com>
2023-03-14 09:03:59 -0700

    stying single card

--- src/components/HomeComponents/Services/Services.jsx
+++ src/components/HomeComponents/Services/Services.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import { cards } from "../../../data/data";
+import SingleCard from "./SingleCard/SingleCard";
 
 const Services = () => {
   const slideLeft = () => {
@@ -14,7 +15,6 @@ const Services = () => {
       slider.scrollLeft = maxScrollLeft;
     }
   };
-
   const slideRight = () => {
     let slider = document.getElementById("slider");
     let maxScrollLeft = slider.scrollWidth - slider.clientWidth; // maximum scroll position
@@ -30,7 +30,29 @@ const Services = () => {
     <section className="py-10">
       <div className="contain">
         <div className="flex flex-col items-start justify-start gap-4 w-full">
-          <h2 className="text-3xl font-semibold text-darkColor">Popular professional services</h2>
+          <h2 className="text-3xl font-bold text-darkColor">
+            Popular professional services
+          </h2>
+          <div className="relative flex items-center group w-full">
+            <MdChevronLeft
+              onClick={slideLeft}
+              size={40}
+              className="bg-white rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+            />
+            <div
+              id="slider"
+              className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
+            >
+              {cards.map((item) => (
+                <SingleCard key={item.id} item={item} />
+              ))}
+            </div>
+            <MdChevronRight
+              onClick={slideRight}
+              size={40}
+              className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden  group-hover:block"
+            />
+          </div>
         </div>
       </div>
     </section>
--- src/components/HomeComponents/Services/SingleCard/SingleCard.jsx
+++ src/components/HomeComponents/Services/SingleCard/SingleCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const SingleCard = ({ item }) => {
-  return <div>SingleCard</div>;
+  return <div className="relative p-2 inline-block cursor-pointer w-[160px] sm:w-[200px] md:w-[240px] lg:w-[250px] bg-red-400 mx-4">SingleCard</div>;
 };
 
 export default SingleCard;
