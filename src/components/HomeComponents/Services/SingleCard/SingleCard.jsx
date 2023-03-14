527b3f323dfc3d34876601ae25ca17236048d928
gift56 <efeasiughu@gmail.com>
2023-03-14 09:20:22 -0700

    stying single card

--- src/components/HomeComponents/Services/SingleCard/SingleCard.jsx
+++ src/components/HomeComponents/Services/SingleCard/SingleCard.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const SingleCard = ({ item }) => {
   return (
     <Link
-      to={`/gigs/${item.id}`}
+      to={`/gigs?cat=design`}
       className="relative p-2 inline-block cursor-pointer w-[160px] sm:w-[200px] md:w-[240px] lg:w-[250px] mx-4"
     >
       <img src={item.img} alt={item.title} className="w-full h-auto block" />
