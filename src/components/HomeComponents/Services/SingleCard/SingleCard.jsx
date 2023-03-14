f1161dbb49fe2c9b0c43bee2c4e22ece5a98d7c8
gift56 <efeasiughu@gmail.com>
2023-03-14 09:16:29 -0700

    stying single card

--- src/components/HomeComponents/Services/SingleCard/SingleCard.jsx
+++ src/components/HomeComponents/Services/SingleCard/SingleCard.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const SingleCard = ({ item }) => {
-  return <div className="relative p-2 inline-block cursor-pointer w-[160px] sm:w-[200px] md:w-[240px] lg:w-[250px] bg-red-400 mx-4">SingleCard</div>;
+  return (
+    <div className="relative p-2 inline-block cursor-pointer w-[160px] sm:w-[200px] md:w-[240px] lg:w-[250px] mx-4">
+      <img src={item.img} alt={item.title} className="w-full h-auto block" />
+    </div>
+  );
 };
 
 export default SingleCard;
--- src/data/data.js
+++ src/data/data.js
@@ -1,4 +1,4 @@
-import image1 from "../assets/images/slide1.png";
+import image from "../assets/images/image.png";
 import image2 from "../assets/images/slide2.png";
 import image3 from "../assets/images/slide3.png";
 import image4 from "../assets/images/slide4.png";
@@ -8,66 +8,73 @@ import image7 from "../assets/images/slide7.png";
 import image8 from "../assets/images/slide8.png";
 import image9 from "../assets/images/slide9.png";
 import image10 from "../assets/images/slide10.png";
+import image11 from "../assets/images/slide1.png";
 
 export const cards = [
   {
     id: "001",
     title: "AI Artists",
     description: "Add talent to AI",
-    img: image1,
+    img: image,
   },
   {
     id: "002",
     title: "Logo Design",
     description: "Build your brand",
-    img: image2,
+    img: image11,
   },
   {
     id: "003",
     title: "WordPress",
     description: "Customize your site",
-    img: image3,
+    img: image2,
   },
   {
     id: "004",
     title: "Voice Over",
     description: "Share your message",
-    img: image4,
+    img: image3,
   },
   {
     id: "005",
     title: "Video Explainer",
     description: "Engage your audience",
-    img: image5,
+    img: image4,
   },
   {
     id: "006",
     title: "Social Media",
     description: "Reach more customers",
-    img: image6,
+    img: image5,
   },
   {
     id: "007",
     title: "SEO",
     description: "Unlock growth online",
-    img: image7,
+    img: image6,
   },
   {
     id: "008",
     title: "Illustration",
     description: "Color your dreams",
-    img: image8,
+    img: image7,
   },
   {
     id: "009",
     title: "Translation",
     description: "Go global",
-    img: image9,
+    img: image8,
   },
   {
     id: "0010",
     title: "Data Entry",
     description: "Learn your business",
+    img: image9,
+  },
+  {
+    id: "0011",
+    title: "Book Covers",
+    description: "Showcase your story",
     img: image10,
   },
 ];
