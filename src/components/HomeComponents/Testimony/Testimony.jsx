c8019754767c70274cdf7c44973bd9208236ea49
gift56 <efeasiughu@gmail.com>
2023-03-14 13:37:57 -0700

    working on testimony page

--- src/components/HomeComponents/Testimony/Testimony.jsx
+++ src/components/HomeComponents/Testimony/Testimony.jsx
@@ -5,7 +5,7 @@ import { BsPlayFill } from "react-icons/bs";
 const Testimony = ({ item }) => {
   return (
     <div className="relative inline-block cursor-pointer w-full mx-4 p-4 pt-0">
-      <div className="w-full flex items-start gap-4 justify-start">
+      <div className="w-full flex items-center gap-7 justify-start">
         <div className="w-[40%] relative">
           <img src={userImg} alt={item.title} className="w-full rounded-md" />
           <span className="absolute top-0 w-full h-full flex items-center justify-center text-white">
@@ -13,6 +13,12 @@ const Testimony = ({ item }) => {
               <BsPlayFill size={59} />
             </span>
           </span>
+        </div>
+        <div className="flex flex-col items-start justify-start gap-5 w-[60%]">
+          <h2 className="text-xl tracking-wide font-semibold text-gray-500">{item.title} | </h2>
+          <p className="text-3xl max-w-[670px] whitespace-normal text-navbg">
+            <i>{item.details}</i>
+          </p>
         </div>
       </div>
     </div>
