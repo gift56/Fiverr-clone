0d6942164e073f6c53fb7c19e16b22724d8cefad
gift56 <efeasiughu@gmail.com>
2023-03-14 14:41:02 -0700

    working on projects

--- src/components/HomeComponents/Projects/Projects.jsx
+++ src/components/HomeComponents/Projects/Projects.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Projects = ({ item }) => {
-  return <div>Projects</div>;
+  return (
+    <Link
+      to={`/`}
+      className="relative inline-block cursor-pointer w-[160px] sm:w-[200px] md:w-[240px] lg:w-[250px] mx-4"
+    >
+      Projects
+    </Link>
+  );
 };
 
 export default Projects;
