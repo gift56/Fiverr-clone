c81cdb6b303e9076c9eb1e30d81bfeda40185d96
gift56 <efeasiughu@gmail.com>
2023-03-15 15:32:23 -0700

    working on gigs cards

--- src/components/GigsContents/GigsCards/GigsCards.jsx
+++ src/components/GigsContents/GigsCards/GigsCards.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { BsStarFill } from "react-icons/bs";
 
 const GigsCards = ({ item }) => {
+  const truckcateString = (str, num) => {
+    if (str?.length > num) {
+      return str.slice(0, num) + "...";
+    } else {
+      return str;
+    }
+  };
   return (
     <Link
       to={`/gigs/${item.id}`}
-      className="w-full shadow-box flex items-start flex-col justify-start border"
+      className="w-full shadow-box flex items-start flex-col justify-start border group"
     >
       <img src={item.img} alt={item.username} className="w-full object-cover" />
       <div className="w-full bg-white p-5 flex items-start flex-col gap-4 justify-start">
@@ -22,7 +30,16 @@ const GigsCards = ({ item }) => {
             <p className="text-sm font-normal text-gray-400">{item.rate}</p>
           </div>
         </div>
-        <p className="text-darkColor text-base">{item.description}</p>
+        <p className="text-darkColor text-base group-hover:text-primary transition-all duration-300">
+          {" "}
+          {truckcateString(item.description, 60)}
+        </p>
+        <p className="flex items-center justify-start gap-1 text-yellow-400 font-semibold">
+          <span>
+            <BsStarFill />
+          </span>
+          5.0
+        </p>
       </div>
     </Link>
   );
