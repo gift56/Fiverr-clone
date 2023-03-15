4b53b9af1eeec1444fc439ad43e2be0b891daaa0
gift56 <efeasiughu@gmail.com>
2023-03-15 15:15:32 -0700

    working on gigs cards

--- src/components/GigsContents/GigsCards/GigsCards.jsx
+++ src/components/GigsContents/GigsCards/GigsCards.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const GigsCards = ({ item }) => {
-  return <Link to={`/gigs/${item.id}`}>GigsCards</Link>;
+  return (
+    <Link
+      to={`/gigs/${item.id}`}
+      className="w-full shadow-box flex items-start flex-col justify-start border"
+    >
+      <img src={item.img} alt={item.username} className="w-full object-cover" />
+      <div className="w-full bg-white p-5 flex items-start flex-col gap-4">
+        
+      </div>
+    </Link>
+  );
 };
 
 export default GigsCards;
