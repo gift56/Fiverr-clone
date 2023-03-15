fa228358ae7c78b50da9ce601efe184b3d3a535c
gift56 <efeasiughu@gmail.com>
2023-03-15 16:03:23 -0700

    working on single gig page

--- src/components/GigsContents/SingleGigContent/GigsDetails.jsx
+++ src/components/GigsContents/SingleGigContent/GigsDetails.jsx
@@ -4,7 +4,13 @@ import { BiChevronRight } from "react-icons/bi";
 const GigsDetails = () => {
   return (
     <div className="w-full flex items-start justify-start gap-3 flex-col">
-      <div className="flex items-center justify-start gap-2"></div>
+      <div className="flex items-center justify-start gap-2 text-gray-500 text-sm font-medium">
+        <span className="text-active">FIVERR </span>
+        <span>
+          <BiChevronRight />
+        </span>
+        <span className="text-active">Web Development</span>
+      </div>
     </div>
   );
 };
