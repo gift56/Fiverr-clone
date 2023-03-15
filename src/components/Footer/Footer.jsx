bb0fd0332d65fef977ce21cdc8d66fc67d7693d8
gift56 <efeasiughu@gmail.com>
2023-03-15 09:54:22 -0700

    working on footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
+  const footerLinks = [
+    {
+      title: "Categories",
+      links: [
+        {
+          to: "/",
+          text: "Graphics & Design",
+        },
+        {
+          to: "/",
+          text: "Digital Marketing",
+        },
+        {
+          to: "/",
+          text: "Writing & Translation",
+        },
+        {
+          to: "/",
+          text: "Video & Animation",
+        },
+        {
+          to: "/",
+          text: "Music & Audio",
+        },
+        {
+          to: "/",
+          text: "Programming & Tech",
+        },
+        {
+          to: "/",
+          text: "Data",
+        },
+        {
+          to: "/",
+          text: "Business",
+        },
+        {
+          to: "/",
+          text: "Lifestyle",
+        },
+        {
+          to: "/",
+          text: "Photography",
+        },
+        {
+          to: "/",
+          text: "Sitemap",
+        },
+      ],
+    },
+  ];
   return (
-    <footer>Footer</footer>
-  )
-}
+    <footer className="border-t w-full py-14">
+      <div className="contain">
+        <div className="w-full flex flex-col items-start justify-start">
+          <div className="w-full"></div>
+          <div></div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
