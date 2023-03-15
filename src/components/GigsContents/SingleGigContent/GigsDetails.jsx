9633586dca1d1b0653899ffbdb1af0ec52805c67
gift56 <efeasiughu@gmail.com>
2023-03-15 16:00:03 -0700

    working on single gig page

--- src/components/GigsContents/SingleGigContent/GigsDetails.jsx
+++ src/components/GigsContents/SingleGigContent/GigsDetails.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const GigsDetails = () => {
   return (
     <div className='w-full flex items-start justify-start gap-3 flex-col'>
-        
+        <div className='flex items-center justify-start gap-2'></div>
     </div>
   )
 }
