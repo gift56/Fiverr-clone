405162d2f638cf22803493caa636fd9874398292
gift56 <efeasiughu@gmail.com>
2023-03-15 13:40:51 -0700

    working on gigs

--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -5,10 +5,10 @@ const Gigs = () => {
   return (
     <main className="py-40">
       <div className="contain">
-        <div className="flex items-start justify-start flex-col gap-5">
+        <div className="flex items-start justify-start flex-col gap-4">
           <div className="flex items-center justify-start gap-4 text-darkColor font-medium">
             <span>
-              <BiHomeAlt size={12}/>
+              <BiHomeAlt size={12} />
             </span>
             <span>/</span>
             <span className="text-sm">Programming & Tech</span>
@@ -16,6 +16,10 @@ const Gigs = () => {
             <span className="text-sm">Website Development</span>
           </div>
           <h2 className="text-3xl font-bold">Web Development</h2>
+          <p className="text-base font-medium">
+            Find a freelance Web development expert to build your
+            Web website
+          </p>
         </div>
       </div>
     </main>
