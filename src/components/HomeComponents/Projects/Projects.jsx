11dc030441fa5943a4132eaf2bdadfbc15866c91
gift56 <efeasiughu@gmail.com>
2023-03-15 11:09:13 -0700

    done with footer

--- src/components/HomeComponents/Projects/Projects.jsx
+++ src/components/HomeComponents/Projects/Projects.jsx
@@ -11,7 +11,7 @@ const Projects = ({ item }) => {
         <img
           src={item.img}
           alt={item.description}
-          className="w-full h-[300px] rounded-tr-md rounded-tl-md object-cover"
+          className="w-full h-[300px] rounded-tr-md rounded-tl-md object-cover hover:opacity-80 transition-all duration-300 rounded-md"
         />
         <div className="w-full flex items-center justify-start gap-2 px-4 rounded-br-md rounded-bl-md bg-white py-4 border">
           <div className="w-10 h-10">
