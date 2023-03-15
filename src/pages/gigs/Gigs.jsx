19f3012ea6a45112e1134b1e6c49d96945e5dccb
gift56 <efeasiughu@gmail.com>
2023-03-15 13:38:44 -0700

    working on gigs

--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { BiHomeAlt } from "react-icons/bi";
 
 const Gigs = () => {
-  return <main className="pt-40">
-    
-  </main>;
+  return (
+    <main className="py-40">
+      <div className="contain">
+        <div className="flex items-start justify-start flex-col gap-5">
+          <div className="flex items-center justify-start gap-4 text-darkColor font-medium">
+            <span>
+              <BiHomeAlt size={12}/>
+            </span>
+            <span>/</span>
+            <span className="text-sm">Programming & Tech</span>
+            <span>/</span>
+            <span className="text-sm">Website Development</span>
+          </div>
+          <h2 className="text-3xl font-bold">Web Development</h2>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Gigs;
