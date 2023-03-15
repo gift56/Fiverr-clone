d9ff2b51f3ffc000c9d9df6f2872221f966f848b
gift56 <efeasiughu@gmail.com>
2023-03-15 15:23:22 -0700

    working on gigs cards

--- src/components/GigsContents/GigsCards/GigsCards.jsx
+++ src/components/GigsContents/GigsCards/GigsCards.jsx
@@ -8,8 +8,21 @@ const GigsCards = ({ item }) => {
       className="w-full shadow-box flex items-start flex-col justify-start border"
     >
       <img src={item.img} alt={item.username} className="w-full object-cover" />
-      <div className="w-full bg-white p-5 flex items-start flex-col gap-4">
-        
+      <div className="w-full bg-white p-5 flex items-start flex-col gap-4 justify-start">
+        <div className="flex items-center justify-start gap-3">
+          <div className="w-8 h-8">
+            <img
+              src={item.userImg}
+              alt={item.username}
+              className="w-full h-full object-cover rounded-full"
+            />
+          </div>
+          <div className="flex flex-col items-start justify-start">
+            <h2 className="text-sm font-medium">{item.username}</h2>
+            <p className="text-sm font-normal text-gray-400">{item.rate}</p>
+          </div>
+        </div>
+        <p className="text-darkColor text-base">{item.description}</p>
       </div>
     </Link>
   );
