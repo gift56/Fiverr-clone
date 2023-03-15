766f9da90ec2f8084335d85635b2d4331835b710
gift56 <efeasiughu@gmail.com>
2023-03-15 15:49:49 -0700

    working on single gig page

--- src/pages/gig/Gig.jsx
+++ src/pages/gig/Gig.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Gig = () => {
   return (
     <main className="py-40">
-      
+      <div className="contain">
+        <div className="w-full flex items-start justify-between"></div>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default Gig
+export default Gig;
