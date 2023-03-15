8f8122589824389debd81622769d65445111ffb7
gift56 <efeasiughu@gmail.com>
2023-03-15 15:55:13 -0700

    working on single gig page

--- src/pages/gig/Gig.jsx
+++ src/pages/gig/Gig.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import GigsDetails from "../../components/GigsContents/SingleGigContent/GigsDetails";
+import GigsOrder from "../../components/GigsContents/SingleGigContent/GigsOrder";
 
 const Gig = () => {
   return (
     <main className="py-40">
       <div className="contain">
         <div className="w-full flex items-start justify-between gap-6">
-          <div className="w-[70%]"></div>
-          <div className="w-[30%]"></div>
+          <div className="w-[70%]">
+            <GigsDetails />
+          </div>
+          <div className="w-[30%]">
+            <GigsOrder />
+          </div>
         </div>
       </div>
     </main>
