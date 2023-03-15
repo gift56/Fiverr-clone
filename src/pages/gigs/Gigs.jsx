8d2619f59b4e9980c5ff5ad0b411273a50230b19
gift56 <efeasiughu@gmail.com>
2023-03-15 14:50:02 -0700

    working on gigs

--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { BiHomeAlt, BiChevronDown } from "react-icons/bi";
 
 const Gigs = () => {
+  const [open, setOpen] = useState(false);
+  const [sort, setSort] = useState("sales");
+
+  const reSort = (types) => {
+    setSort(types);
+    setOpen(false);
+  };
   return (
     <main className="py-40">
       <div className="contain">
@@ -23,21 +30,50 @@ const Gigs = () => {
             <div className="flex items-center justify-start gap-2">
               <p className="text-base font-normal text-gray-500">Budget:</p>
               <form className="flex items-center justify-start gap-2">
-                <input type="text" placeholder="min" className="border w-[150px] outline-none px-2 h-[40px] rounded-md text-gray-500" />
-                <input type="text" placeholder="max" className="border w-[150px] outline-none px-2 h-[40px] rounded-md text-gray-500" />
-                <button className="w-fit bg-primary text-white text-base font-medium py-2 px-7 outline-none rounded-md hover:bg-primary/95">Apply</button>
+                <input
+                  type="text"
+                  placeholder="min"
+                  className="border w-[150px] outline-none px-2 h-[40px] rounded-md text-gray-500"
+                />
+                <input
+                  type="text"
+                  placeholder="max"
+                  className="border w-[150px] outline-none px-2 h-[40px] rounded-md text-gray-500"
+                />
+                <button className="w-fit bg-primary text-white text-base font-medium py-2 px-7 outline-none rounded-md hover:bg-primary/95">
+                  Apply
+                </button>
               </form>
             </div>
             <div className="flex items-center justify-end gap-2">
               <p className="text-base font-normal text-gray-500">SortBy:</p>
               <div className="flex items-center justify-start gap-2 cursor-pointer relative">
-                <p className="text-sm">Best Selling</p>
-                <span>
-                  <BiChevronDown size={20} />
-                </span>
-                <div className={`flex flex-col items-start justify-start bg-white shadow-box rounded-md absolute w-[140px] top-8 right-2`}>
-                  <div className="px-4 py-2 w-full border-b text-gray-500 text-sm cursor-pointer hover:bg-darkColor/40 hover:text-white rounded-tr-md rounded-tl-md">Newest</div>
-                  <div className="px-4 py-2 w-full border-b text-gray-500 text-sm cursor-pointer hover:bg-darkColor/40 hover:text-white rounded-br-md rounded-bl-md">Best Selling</div>
+                <div
+                  className="flex items-center justify-start gap-2 cursor-pointer relative"
+                  onClick={() => setOpen((prev) => !prev)}
+                >
+                  <p className="text-sm capitalize">{sort}</p>
+                  <span>
+                    <BiChevronDown size={20} />
+                  </span>
+                </div>
+                <div
+                  className={`${
+                    open ? "flex" : "hidden"
+                  } flex-col items-start justify-start bg-white shadow-box rounded-md absolute w-[140px] top-8 right-2`}
+                >
+                  <div
+                    onClick={() => reSort("createdAt")}
+                    className="px-4 py-2 w-full border-b text-gray-500 text-sm cursor-pointer hover:bg-darkColor/40 hover:text-white rounded-tr-md rounded-tl-md"
+                  >
+                    Newest
+                  </div>
+                  <div
+                    onClick={() => reSort("sales")}
+                    className="px-4 py-2 w-full border-b text-gray-500 text-sm cursor-pointer hover:bg-darkColor/40 hover:text-white rounded-br-md rounded-bl-md"
+                  >
+                    sales
+                  </div>
                 </div>
               </div>
             </div>
