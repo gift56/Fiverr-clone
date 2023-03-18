20d01ef9e4e575fba715454b3b4dafcb5a5d1ca2
gift56 <efeasiughu@gmail.com>
2023-03-18 16:45:02 -0700

    working on order page

--- src/pages/orders/Orders.jsx
+++ src/pages/orders/Orders.jsx
@@ -3,7 +3,79 @@ import { CiMail } from "react-icons/ci";
 import { columns, tableData } from "../../data/data";
 
 const Orders = () => {
-  return <div>Orders</div>;
+  const tableActions = tableData.map((item) => ({
+    image: (
+      <div className="w-14 h-10">
+        <img
+          src={item.img}
+          alt={item.title}
+          className="w-full h-full object-cover"
+        />
+      </div>
+    ),
+    title: (
+      <p className="w-full flex items-center justify-start">{item.title}</p>
+    ),
+    price: (
+      <p className="w-full flex items-center justify-start">{item.price}</p>
+    ),
+    orders: (
+      <p className="w-full flex items-center justify-start">{item.sales}</p>
+    ),
+    actions: (
+      <div
+        className="w-10 h-10 flex items-center justify-start  cursor-pointer bg-blue-600"
+        onClick={() => alert(item.title)}
+      >
+        <CiMail size={18} />
+      </div>
+    ),
+  }));
+
+  return (
+    <main className="py-40">
+      <div className="contain">
+        <div className="w-full flex flex-col items-start gap-5 justify-start">
+          <div className="flex items-center justify-between w-full gap-2">
+            <h2 className="text-2xl font-bold">Orders</h2>
+          </div>
+          <table className="w-full">
+            <thead className="h-[35px]">
+              <tr className="border-b">
+                {columns &&
+                  columns.map((head, i) => (
+                    <th
+                      key={i}
+                      className="text-left text-gray-700 text-sm font-semibold leading-[18px] pb-2"
+                    >
+                      {head.header}
+                    </th>
+                  ))}
+              </tr>
+            </thead>
+            <tbody className="w-full">
+              {tableActions &&
+                tableActions.map((row, i) => (
+                  <tr
+                    key={i}
+                    className="text-sm leading-5 w-full even:bg-gray-200"
+                  >
+                    {columns?.map((col, i) => (
+                      <td
+                        key={i}
+                        className="first:text-left text-sm text-darkColor font-medium text-center py-2"
+                      >
+                        {row[col.field]}
+                      </td>
+                    ))}
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Orders;
