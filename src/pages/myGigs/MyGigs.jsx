702261272d15f798c57a9698a04756ec008af084
gift56 <efeasiughu@gmail.com>
2023-03-18 15:56:10 -0700

    working on my gigs page

--- src/pages/myGigs/MyGigs.jsx
+++ src/pages/myGigs/MyGigs.jsx
@@ -4,7 +4,9 @@ const MyGigs = () => {
   return (
     <main className="py-40">
       <div className="contain">
-        <div className="w-full flex flex-col items-start gap-5"></div>
+        <div className="w-full flex flex-col items-start gap-5 justify-start">
+          <div className="flex items-center justify-between w-full gap-2"></div>
+        </div>
       </div>
     </main>
   );
