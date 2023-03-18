ed915a78f0d19c0cc7740b3c4e2308679e5a2878
gift56 <efeasiughu@gmail.com>
2023-03-18 16:56:52 -0700

    working on order page

--- src/pages/messages/Messages.jsx
+++ src/pages/messages/Messages.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const Messages = () => {
   return (
     <main className="py-40">
-      <div className="contain">Messages</div>
+      <div className="contain">
+        <div className="w-full flex flex-col items-start gap-5 justify-start">
+          <h2 className="text-2xl font-bold">Messages</h2>
+        </div>
+      </div>
     </main>
   );
 };
