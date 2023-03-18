c5c4040cd3cfabac8070d5b081f1bc2e08914317
gift56 <efeasiughu@gmail.com>
2023-03-18 16:34:59 -0700

    working on my gigs page

--- src/pages/myGigs/MyGigs.jsx
+++ src/pages/myGigs/MyGigs.jsx
@@ -1,69 +1,8 @@
 import React from "react";
 import { BsTrash } from "react-icons/bs";
+import { columns, tableData } from "../../data/data";
 
 const MyGigs = () => {
-  const tableData = [
-    {
-      img: "https://fiverr-res.cloudinary.com/f_auto,q_auto/v1/attachments/generic_asset/asset/c8c0b94d835437aaa18685a80831010a-1675613384041/Website%20Developer%20med2.jpeg",
-      title: "Mara Andre",
-      price: "20",
-      sales: "102",
-    },
-    {
-      img: "https://fiverr-res.cloudinary.com/f_auto,q_auto/v1/attachments/generic_asset/asset/c8c0b94d835437aaa18685a80831010a-1675613384041/Website%20Developer%20med2.jpeg",
-      title: "Mara Andre",
-      price: "20",
-      sales: "102",
-    },
-    {
-      img: "https://fiverr-res.cloudinary.com/f_auto,q_auto/v1/attachments/generic_asset/asset/c8c0b94d835437aaa18685a80831010a-1675613384041/Website%20Developer%20med2.jpeg",
-      title: "Mara Andre",
-      price: "20",
-      sales: "102",
-    },
-    {
-      img: "https://fiverr-res.cloudinary.com/f_auto,q_auto/v1/attachments/generic_asset/asset/c8c0b94d835437aaa18685a80831010a-1675613384041/Website%20Developer%20med2.jpeg",
-      title: "Mara Andre",
-      price: "20",
-      sales: "102",
-    },
-    {
-      img: "https://fiverr-res.cloudinary.com/f_auto,q_auto/v1/attachments/generic_asset/asset/c8c0b94d835437aaa18685a80831010a-1675613384041/Website%20Developer%20med2.jpeg",
-      title: "Mara Andre",
-      price: "20",
-      sales: "102",
-    },
-    {
-      img: "https://fiverr-res.cloudinary.com/f_auto,q_auto/v1/attachments/generic_asset/asset/c8c0b94d835437aaa18685a80831010a-1675613384041/Website%20Developer%20med2.jpeg",
-      title: "Mara Andre",
-      price: "20",
-      sales: "102",
-    },
-  ];
-
-  const columns = [
-    {
-      field: "image",
-      header: "Image",
-    },
-    {
-      field: "title",
-      header: "Title",
-    },
-    {
-      field: "price",
-      header: "Price",
-    },
-    {
-      field: "orders",
-      header: "Orders",
-    },
-    {
-      field: "actions",
-      header: "Actions",
-    },
-  ];
-
   const tableActions = tableData.map((item) => ({
     image: (
       <div className="w-14 h-10">
