5e5b24446e5a76b77aa28fef944d1740f05348b9
gift56 <efeasiughu@gmail.com>
2023-03-18 14:45:51 -0700

    working on order box

--- src/components/GigsContents/SingleGigContent/GigsOrder.jsx
+++ src/components/GigsContents/SingleGigContent/GigsOrder.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const GigsOrder = () => {
   return (
-    <div className='w-full'>GigsOrder</div>
-  )
-}
+    <div className="w-full bg-white border p-4 flex flex-col gap-4 items-start justify-start rounded">
+      <div className="flex items-center justify-between gap-2 w-full">
+        <h2 className="text-gray-800 text-sm font-bold">1 AI generated image</h2>
+        <p className="text-base font-normal">$ 50.99</p>
+      </div>
+    </div>
+  );
+};
 
-export default GigsOrder
+export default GigsOrder;
