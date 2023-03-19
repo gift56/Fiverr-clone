a95910c801e528be5c8218ea9656163fd3c716ae
gift56 <efeasiughu@gmail.com>
2023-03-19 14:47:49 -0700

    working on add page

--- src/pages/add/Add.jsx
+++ src/pages/add/Add.jsx
@@ -4,8 +4,8 @@ const Add = () => {
   return (
     <main className="py-40">
       <div className="contain">
-        <div className="flex flex-col w-full gap-4">
-          
+        <div className="flex flex-col w-full gap-4 items-start justify-start">
+
         </div>
       </div>
     </main>
