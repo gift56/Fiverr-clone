551e4f38b300824890f7cb41a70085cca2073204
gift56 <efeasiughu@gmail.com>
2023-03-19 15:31:55 -0700

    working on add page

--- src/pages/add/Add.jsx
+++ src/pages/add/Add.jsx
@@ -26,7 +26,7 @@ const Add = () => {
         <div className="flex flex-col w-full gap-4 items-start justify-start">
           <h2 className="text-2xl font-bold">Add New Gig</h2>
           <div className="flex items-start justify-between gap-4 w-full">
-            <div className="flex-1 flex items-start justify-start flex-col gap-2 ">
+            <div className="flex-1 flex items-start justify-start flex-col gap-2">
               <div className="flex flex-col w-full gap-1 items-start justify-start">
                 <label htmlFor="title" className="text-sm font-medium">
                   Title
