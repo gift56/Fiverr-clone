edeadf9d12168aa2661b71a5d16f6c451d6cf62e
gift56 <efeasiughu@gmail.com>
2023-03-19 14:47:24 -0700

    working on add

--- src/pages/add/Add.jsx
+++ src/pages/add/Add.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const Add = () => {
   return (
     <main className="py-40">
-      <div className="contain">Add</div>
+      <div className="contain">
+        <div className="flex flex-col w-full gap-4">
+          
+        </div>
+      </div>
     </main>
   );
 };
