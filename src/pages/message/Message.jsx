69f936dcb54f80177b4d63c6b1b2777409de3d99
gift56 <efeasiughu@gmail.com>
2023-03-18 18:07:46 -0700

    done working on message page

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -15,7 +15,7 @@ const Message = () => {
             <span>John Doe</span> <BiChevronRight size={20} />
           </p>
           <div className="w-[80%] flex items-start justify-start gap-4 flex-col">
-            <div className="h-[400px] overflow-auto border">
+            <div className="h-[400px] overflow-auto border p-4">
               <div className="item">
                 <img
                   src="https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto/v1/attachments/generic_asset/asset/c51dbf5c160dac9bd067442911e65d16-1626181910196/Miscellaneous_2x.png"
