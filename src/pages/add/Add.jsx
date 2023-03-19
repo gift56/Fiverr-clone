a85f8bf857fbcb07188889e11ae6e096fd799d4d
gift56 <efeasiughu@gmail.com>
2023-03-19 14:49:34 -0700

    working on add page

--- src/pages/add/Add.jsx
+++ src/pages/add/Add.jsx
@@ -6,6 +6,10 @@ const Add = () => {
       <div className="contain">
         <div className="flex flex-col w-full gap-4 items-start justify-start">
           <h2 className="text-2xl font-bold">Add New Gig</h2>
+          <div className="flex items-start justify-between gap-4 w-full">
+            <div className="flex-1"></div>
+            <div className="flex-1"></div>
+          </div>
         </div>
       </div>
     </main>
