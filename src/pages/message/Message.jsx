ed1352137ff9d5575c7a3d347161c71f9f43b05c
gift56 <efeasiughu@gmail.com>
2023-03-18 17:43:26 -0700

    done working on message page

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Message = () => {
   return (
     <main className="py-40">
       <div className="contain">
-        <span></span>
+        <span>
+          <Link to="/messages"></Link>
+        </span>
       </div>
     </main>
   );
