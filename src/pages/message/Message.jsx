21f3240ce8ea3ca073bef9684d4a7c80afd4197d
gift56 <efeasiughu@gmail.com>
2023-03-18 18:34:48 -0700

    done working on message page

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BiChevronRight } from "react-icons/bi";
 import { Link } from "react-router-dom";
+import { mesagesData } from "../../data/data";
 
 const Message = () => {
   return (
@@ -16,36 +17,34 @@ const Message = () => {
           </p>
           <div className="w-[80%] flex items-start justify-start gap-4 flex-col h-[500px]">
             <div className="h-[500px] overflow-auto border p-4 rounded-md flex flex-col gap-4 w-full">
-              <div className="flex items-start gap-5 max-w-[600px]">
-                <img
-                  src="https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto/v1/attachments/generic_asset/asset/c51dbf5c160dac9bd067442911e65d16-1626181910196/Miscellaneous_2x.png"
-                  alt="sender_img"
-                  className="w-10 h-10 object-cover rounded-full border"
-                />
-                <p className="text-sm bg-gray-200 p-4 rounded-tl-[0] rounded-2xl">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Perspiciatis beatae aperiam cupiditate aliquid reprehenderit
-                  voluptatem qui asperiores illo atque suscipit odio, alias
-                  doloribus repellat voluptates nesciunt distinctio! Tempora
-                  labore, voluptate hic repellat ad deserunt alias earum
-                  recusandae nulla ullam repellendus.
-                </p>
-              </div>
-              <div className="flex items-start gap-5 max-w-[600px] flex-row-reverse self-end">
-                <img
-                  src="https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto/v1/attachments/generic_asset/asset/c51dbf5c160dac9bd067442911e65d16-1626181910196/Miscellaneous_2x.png"
-                  alt="sender_img"
-                  className="w-10 h-10 object-cover rounded-full border"
-                />
-                <p className="text-sm bg-blue-500 p-4 rounded-tr-[0] rounded-2xl text-white">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Perspiciatis beatae aperiam cupiditate aliquid reprehenderit
-                  voluptatem qui asperiores illo atque suscipit odio, alias
-                  doloribus repellat voluptates nesciunt distinctio! Tempora
-                  labore, voluptate hic repellat ad deserunt alias earum
-                  recusandae nulla ullam repellendus.
-                </p>
-              </div>
+              {mesagesData.map((item, i) => (
+                <div
+                  key={i}
+                  className={`flex items-start gap-5 max-w-[600px] ${
+                    item.owner === true ? "self-end flex-row-reverse" : ""
+                  }`}
+                >
+                  <img
+                    src="https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto/v1/attachments/generic_asset/asset/c51dbf5c160dac9bd067442911e65d16-1626181910196/Miscellaneous_2x.png"
+                    alt="sender_img"
+                    className="w-10 h-10 object-cover rounded-full border"
+                  />
+                  <p
+                    className={`text-sm rounded-2xl text-white ${
+                      item.owner === true
+                        ? "bg-blue-500 p-4 rounded-tr-[0]"
+                        : "bg-gray-200 rounded-tl-[0]"
+                    }`}
+                  >
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Perspiciatis beatae aperiam cupiditate aliquid reprehenderit
+                    voluptatem qui asperiores illo atque suscipit odio, alias
+                    doloribus repellat voluptates nesciunt distinctio! Tempora
+                    labore, voluptate hic repellat ad deserunt alias earum
+                    recusandae nulla ullam repellendus.
+                  </p>
+                </div>
+              ))}
             </div>
             <div className="flex items-end w-full gap-8">
               <textarea
