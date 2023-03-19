6d53f498ea77d6935f1945ea1fcbec89a3d411dc
gift56 <efeasiughu@gmail.com>
2023-03-19 15:07:43 -0700

    working on add page

--- src/pages/add/Add.jsx
+++ src/pages/add/Add.jsx
@@ -27,7 +27,7 @@ const Add = () => {
           <div className="flex items-start justify-between gap-4 w-full">
             <div className="flex-1 flex items-start justify-start flex-col gap-2 ">
               <div className="flex flex-col w-full gap-1 items-start justify-start">
-                <label htmlFor="title">Title</label>
+                <label htmlFor="title" className="text-sm font-medium">Title</label>
                 <input
                   type="text"
                   name="title"
@@ -49,6 +49,16 @@ const Add = () => {
                     </option>
                   ))}
                 </select>
+              </div>
+              <div className="flex flex-col w-full gap-1 items-start justify-start">
+                <label htmlFor="title" className="text-sm font-medium">Cover Image</label>
+                <input
+                  type="text"
+                  name="title"
+                  id="title"
+                  placeholder="e.g I will do something I'm really good at..."
+                  className="border w-full h-10 px-3 rounded-md outline-none text-sm"
+                />
               </div>
             </div>
             <div className="flex-1"></div>
