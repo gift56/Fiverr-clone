10f5ac467e42907be5b5da4bc737721d07db464a
gift56 <efeasiughu@gmail.com>
2023-03-18 17:52:09 -0700

    done working on message page

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -6,14 +6,27 @@ const Message = () => {
   return (
     <main className="py-40">
       <div className="contain">
-        <p className="flex items-center gap-2 uppercase justify-start text-sm font-medium text-darkColor/60">
-          <Link to="/messages" className="font-semibold text-darkColor">MESSAGES</Link> <BiChevronRight size={20} />
-          <span>John Doe</span> <BiChevronRight size={20} />
-        </p>
-        <div className="message"></div>
-        <div className="write">
-          <textarea name="message" placeholder="Reply a message" cols="30" rows="10"></textarea>
-          <button>Send</button>
+        <div className="w-full flex items-center justify-center flex-col">
+          <p className="flex items-center gap-2 uppercase justify-start text-sm font-medium text-darkColor/60 w-[80%]">
+            <Link to="/messages" className="font-semibold text-darkColor">
+              MESSAGES
+            </Link>{" "}
+            <BiChevronRight size={20} />
+            <span>John Doe</span> <BiChevronRight size={20} />
+          </p>
+          <div className="w-[80%]">
+            <div className="message"></div>
+            <div className="flex items-end w-full gap-8">
+              <textarea
+                name="message"
+                placeholder="Reply a message"
+                cols="30"
+                rows="10"
+                className="border"
+              ></textarea>
+              <button>Send</button>
+            </div>
+          </div>
         </div>
       </div>
     </main>
