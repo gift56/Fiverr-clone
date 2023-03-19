6038d14770252ad8219700701192c590c582692a
gift56 <efeasiughu@gmail.com>
2023-03-19 14:54:57 -0700

    working on add page

--- src/pages/add/Add.jsx
+++ src/pages/add/Add.jsx
@@ -7,10 +7,10 @@ const Add = () => {
         <div className="flex flex-col w-full gap-4 items-start justify-start">
           <h2 className="text-2xl font-bold">Add New Gig</h2>
           <div className="flex items-start justify-between gap-4 w-full">
-            <div className="flex-1 flex items-start justify-start flex-col gap-2">
-              <div className="flex flex-col w-full gap-2 items-start justify-start">
+            <div className="flex-1 flex items-start justify-start flex-col gap-2 ">
+              <div className="flex flex-col w-full gap-1 items-start justify-start">
                 <label htmlFor="title">Title</label>
-                <input type="text" name="title" id="title" />
+                <input type="text" name="title" id="title" placeholder="e.g I will do something I'm really good at..." className="border w-full h-10 px-3 rounded-md outline-none" />
               </div>
             </div>
             <div className="flex-1"></div>
