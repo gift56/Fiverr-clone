4766a48c0150d5b8bb660bb2473970db6de9c1ff
gift56 <efeasiughu@gmail.com>
2023-03-18 17:41:56 -0700

    working on message page

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Message = () => {
-  return <main className="py-10">Message</main>;
+  return (
+    <main className="py-40">
+      <div className="contain">
+        
+      </div>
+    </main>
+  );
 };
 
 export default Message;
