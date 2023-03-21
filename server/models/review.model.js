71dff7ab574a2bab92fd158c7d6ecb80e12c839f
gift56 <efeasiughu@gmail.com>
2023-03-21 15:04:04 -0700

    working on models

--- server/models/review.model.js
+++ server/models/review.model.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const OrderSchema = new Schema(
+const ReviewSchema = new Schema(
   {},
   {
     timestamps: true,
   }
 );
 
-export default mongoose.model("Order", OrderSchema);
+export default mongoose.model("Review", ReviewSchema);
