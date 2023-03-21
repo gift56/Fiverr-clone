0f84612c3b9a3f7b2a59bcd488d5e41bfe35a348
gift56 <efeasiughu@gmail.com>
2023-03-21 16:47:07 -0700

    done importing routes

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -3,6 +3,7 @@ import {  } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.get("/test", );
+router.get("/register", );
+router.get("/login", );
 
 export default router;
