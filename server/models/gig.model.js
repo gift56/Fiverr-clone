a27ac67a63df51f84e0930b6396c9f3daf94beaf
gift56 <efeasiughu@gmail.com>
2023-03-21 14:52:06 -0700

    working on gig model

--- server/models/gig.model.js
+++ server/models/gig.model.js
@@ -2,7 +2,20 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const GigSchema = new Schema(
-  {},
+  {
+    userId: {
+      type: String,
+      required,
+    },
+    title: {
+      type: String,
+      required,
+    },
+    userId: {
+      type: String,
+      required,
+    },
+  },
   {
     timestamps: true,
   }
