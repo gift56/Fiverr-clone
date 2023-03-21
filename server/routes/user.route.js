a0638a33ee29899d31642a67b32be8e746ca7350
gift56 <efeasiughu@gmail.com>
2023-03-21 16:05:51 -0700

    working on user endpoints

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,9 +1,8 @@
 import express from "express";
+import { deleteUser } from "../controllers/user.controller";
 
 const router = express.Router();
 
-router.get("/test", (req, res) => {
-  res.send("It works");
-});
+router.get("/test", deleteUser);
 
 export default router;
