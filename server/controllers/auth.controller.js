358252a508e08e416fdfc833a11ffe5c49bef7eb
gift56 <efeasiughu@gmail.com>
2023-03-21 16:52:54 -0700

    working on suthentication routes

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -5,3 +5,7 @@ export const register = (req, res) => {
 export const login = (req, res) => {
     res.send("It worked")
 };
+
+export const logout = (req, res) => {
+    res.send("It worked")
+};
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { login, register } from "../controllers/auth.controller.js";
+import { login, logout, register } from "../controllers/auth.controller.js";
 
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
