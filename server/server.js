43aea1441c673cb87715b92cfdfeebe2a3f3eade
gift56 <efeasiughu@gmail.com>
2023-03-21 16:42:20 -0700

    importing routes

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import gigRoute from "./routes/gig.route.js";
 import orderRoute from "./routes/order.route.js";
 import converstionRoute from "./routes/conversation.route.js";
 import messageRoute from "./routes/message.route.js";
+import reviewRoute from "./routes/review.route.js";
 
 const app = express();
 dotenv.config();
