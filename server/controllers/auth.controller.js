c41eb3aa5277f6d0c4d55e3168aa5baf3b2e324c
gift56 <efeasiughu@gmail.com>
2023-03-21 16:55:08 -0700

    working on suthentication routes

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,4 +1,9 @@
-export const register = async (req, res) => {};
+export const register = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).send("Something went wrong!");
+  }
+};
 
 export const login = async (req, res) => {};
 
