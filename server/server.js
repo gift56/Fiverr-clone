d11466abb4e7cc0a052fa7c11ba738d92d0c9acf
gift56 <efeasiughu@gmail.com>
2023-03-20 20:21:09 -0700

    working on server back

--- server/server.js
+++ server/server.js
@@ -3,6 +3,14 @@ import mongoose from "mongoose";
 
 const app = express();
 
+try {
+  await mongoose.connect(
+    "mongodb+srv://efegift:<password>@cluster0.jly2ewj.mongodb.net/?retryWrites=true&w=majority"
+  );
+} catch (error) {
+  handleError(error);
+}
+
 app.listen(8000, () => {
   console.log(`Server running on port http://localhost:${8000}`);
 });
