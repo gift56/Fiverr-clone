71776d5946eecdfb585bdbf1334577dbd8a0b3a4
gift56 <efeasiughu@gmail.com>
2023-03-20 20:42:28 -0700

    working on models

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,8 +1,9 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  username:{
-    
-  }
+  username: {
+    type: String,
+    required: true,
+  },
 });
