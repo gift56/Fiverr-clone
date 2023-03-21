f72a4eb203600c1f63751b404c5fd297926d7bb6
gift56 <efeasiughu@gmail.com>
2023-03-21 14:41:50 -0700

    working on gig model

---
+++ server/models/gig.model.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const GigSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: false,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: false,
+    },
+    desc: {
+      type: String,
+      required: false,
+    },
+    isSeller: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Gig", GigSchema);
