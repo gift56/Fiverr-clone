35d8cb66acb764ceb2bc180539a4ffc29d882dd7
gift56 <efeasiughu@gmail.com>
2023-03-21 16:56:49 -0700

    working on authentication routes

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,8 @@
+import User from "../models/user.model.js";
+
 export const register = async (req, res) => {
   try {
+    const newUser = User;
   } catch (error) {
     res.status(500).send("Something went wrong!");
   }
