0a6dbbf925c97e1e72c803a802c770fda062946c
gift56 <efeasiughu@gmail.com>
2023-03-21 16:48:43 -0700

    working on suthentication routes

---
+++ server/routes/auth.route.js
@@ -0,0 +1,9 @@
+import express from "express";
+import {  } from "../controllers/user.controller.js";
+
+const router = express.Router();
+
+router.get("/register", );
+router.get("/login", );
+
+export default router;
