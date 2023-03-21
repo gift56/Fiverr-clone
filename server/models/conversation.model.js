90994977355ddbbdd784310ef6f5c1bf3a439348
gift56 <efeasiughu@gmail.com>
2023-03-21 15:32:54 -0700

    working on conversation model

--- server/models/conversation.model.js
+++ server/models/conversation.model.js
@@ -2,7 +2,18 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const ConversationSchema = new Schema(
-  {},
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
   {
     timestamps: true,
   }
