6a37f32f834f7ee88b35e709ca2b942e3a30bed6
gift56 <efeasiughu@gmail.com>
2023-03-21 16:01:31 -0700

    working on user endpoints

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -2,8 +2,8 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("test",(req,res)=>{
-    
-})
+router.get("test", (req, res) => {
+    res.send("It works")
+});
 
-export default router
+export default router;
