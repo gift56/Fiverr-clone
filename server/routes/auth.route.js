f4a4067b5d0392125edfce1ab6e2ef1084e887be
gift56 <efeasiughu@gmail.com>
2023-03-21 17:09:37 -0700

    working on authentication routes

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,9 +1,7 @@
 import express from "express";
 import { login, logout, register } from "../controllers/auth.controller.js";
 
-
 const router = express.Router();
-
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
