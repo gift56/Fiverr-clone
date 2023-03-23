f76acb0249d10135e67074c0a75706d13412a499
gift56 <efeasiughu@gmail.com>
2023-03-23 12:23:23 -0700

    working on login endpoint

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -19,7 +19,13 @@ export const register = async (req, res) => {
 export const login = async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-    if(!user) return res.status(404).send("User not found!")
+    if (!user) return res.status(404).send("User not found!");
+    const isCorrectPassword = bcrypt.compareSync(
+      req.body.password,
+      user.password
+    );
+    if (!isCorrectPassword) return res.status(400).send("Invalid credentials!");
+    
   } catch (error) {
     res.status(500).send("Something went wrong!");
   }
