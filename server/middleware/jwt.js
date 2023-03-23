39a5bcdb49646b589cf9f57c2374f14ba1342c7f
gift56 <efeasiughu@gmail.com>
2023-03-23 15:41:07 -0700

    done with error middlewares

--- server/middleware/jwt.js
+++ server/middleware/jwt.js
@@ -1,11 +1,13 @@
 import jwt from "jsonwebtoken";
+import createError from "../utils/createError.js";
 
-export const verifyToken = (req, res) => {
+export const verifyToken = (req, res, next) => {
   const token = req.cookies.accessToken;
-  if (!token) return res.status(401).send("User not authenticated!");
+  if (!token) return next(createError(401, "User not authenticated!"));
   jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
-    if (err) return res.status(403).send("Invalid access token");
+    if (err) return next(createError(403, "Invalid access token"));
     req.userId = payload.id;
     req.isSeller = payload.isSeller;
+    next();
   });
 };
