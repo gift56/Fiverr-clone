330b1f3fb1a5dc77c2c333c8a58faa646af1e793
gift56 <efeasiughu@gmail.com>
2023-03-23 16:07:16 -0700

    working on logged in page

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,5 +1,7 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useRef, useEffect } from "react";
 import { motion } from "framer-motion";
+import { useFormik } from "formik";
+import * as yup from "yup";
 
 const Login = ({ show, setShow }) => {
   const modalRef = useRef(null);
@@ -15,6 +17,37 @@ const Login = ({ show, setShow }) => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
+
+  const initialValues = {
+    username: "",
+    password: "",
+  };
+
+  const validationSchema = yup.object({
+    username: yup.string().required("Required"),
+    password: yup
+      .string()
+      .min(5, "Password must be at least 5 characters long")
+      .required("Required"),
+  });
+
+  const onSubmit = async (payload, actions) => {
+    request(payload);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    actions.resetForm();
+    setShow(false);
+  };
+
+  const { handleChange, values, handleBlur, handleSubmit, errors, touched } =
+    useFormik({
+      initialValues,
+      validationSchema,
+      onSubmit,
+    });
+
+  const getError = (key) => {
+    return touched[key] && errors[key];
+  };
 
   return (
     <section className="">
--- client/src/utils/Input/CustomizeInput.jsx
+++ client/src/utils/Input/CustomizeInput.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
 
-const CustomizeInput = () => {
+const CustomizeInput = ({
+  htmlFor,
+  showLabel,
+  id,
+  label,
+  className,
+  labelClassName,
+  inpuClassName,
+  error,
+  onChange,
+  onBlur,
+  accept,
+  errorClass,
+  containerClass,
+  ...props
+}) => {
   return (
-    <div>CustomizeInput</div>
-  )
-}
+    <div className={`flex flex-col gap-1 w-full ${containerClass}`}>
+      {showLabel === false && (
+        <label htmlFor={htmlFor} className={labelClassName}>
+          {label}
+        </label>
+      )}
+      <div className={`flex flex-col gap-1 w-full ${inpuClassName}`}>
+        <input
+          {...props}
+          className={`${className} ${error ? "border !border-red-400" : ""}`}
+          id={id}
+          onBlur={onBlur}
+          onChange={onChange}
+          accept={accept}
+        />
+        {error ? (
+          <p className={`text-xs text-red-600 ${errorClass}`}>{error}</p>
+        ) : null}
+      </div>
+    </div>
+  );
+};
 
-export default CustomizeInput
+export default CustomizeInput;
