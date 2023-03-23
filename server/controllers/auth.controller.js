a4a905b24b5a3b5f1c4c264b7a308dd6adee237b
gift56 <efeasiughu@gmail.com>
2023-03-23 12:08:10 -0700

    working on login endpoint

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -5,18 +5,23 @@ export const register = async (req, res) => {
   try {
     const hash = bcrypt.hashSync(req.body.password, 5);
     const newUser = new User({
-        ...req.body,
-        password: hash,
-      });
+      ...req.body,
+      password: hash,
+    });
 
     await newUser.save();
     res.status(201).send("Registration Successful.");
   } catch (error) {
-    console.log(error.message);
     res.status(500).send("Something went wrong!");
   }
 };
 
-export const login = async (req, res) => {};
+export const login = async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+  } catch (error) {
+    res.status(500).send("Something went wrong!");
+  }
+};
 
 export const logout = async (req, res) => {};
