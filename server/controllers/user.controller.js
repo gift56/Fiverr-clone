3498064dbb6cdbb7912d336703f31f2e9f266500
gift56 <efeasiughu@gmail.com>
2023-03-23 14:44:33 -0700

    working on delete user endpoint

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -6,9 +6,10 @@ export const deleteUser = async (req, res) => {
   const token = req.cookies.accessToken;
   if (!token) return res.status(401).send("User not authenticated!");
   jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
-    if (payload.id !== user._id) {
+    if (payload.id !== user._id.toString()) {
       return res.status(403).send("You can delete only your account!");
     }
     await User.findByIdAndDelete(req.params.id);
+    res.status(200).send("Deleted Sucessfully");
   });
 };
