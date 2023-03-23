756b5eb297b572b3739ad2bc3aa039397459550e
gift56 <efeasiughu@gmail.com>
2023-03-23 15:04:46 -0700

    creating a middleware fot jwt token

--- server/middleware/jwt.js
+++ server/middleware/jwt.js
@@ -1,4 +1,9 @@
 export const verifyToken = (req, res) => {
   const token = req.cookies.accessToken;
   if (!token) return res.status(401).send("User not authenticated!");
+  jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
+    if (err) return res.status(403).send("Invalid access token");
+    req.userId = payload.id;
+    req.isSeller = payload.isSeller;
+  });
 };
