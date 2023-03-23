462f17aa4b0d5297ca179a43f31d95d54f8d6a75
gift56 <efeasiughu@gmail.com>
2023-03-23 15:47:05 -0700

    done with error middlewares

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -7,5 +7,5 @@ export const deleteUser = async (req, res, next) => {
     return next(createError(403, "You can delete only your account!"));
   }
   await User.findByIdAndDelete(req.params.id);
-  next(createError(200, "Deleted Sucessfully."));
+  res.status(200).send("Deleted Sucessfully.");
 };
