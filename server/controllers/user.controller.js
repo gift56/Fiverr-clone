58adbd8c8af7dffcf6594a798d9c775ea45051e7
gift56 <efeasiughu@gmail.com>
2023-03-23 15:42:23 -0700

    done with error middlewares

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,10 +1,11 @@
 import User from "../models/user.model.js";
+import createError from "../utils/createError.js";
 
-export const deleteUser = async (req, res) => {
+export const deleteUser = async (req, res, next) => {
   const user = await User.findById(req.params.id);
-    if (req.userId !== user._id.toString()) {
-      return res.status(403).send("You can delete only your account!");
-    }
-    await User.findByIdAndDelete(req.params.id);
-    res.status(200).send("Deleted Sucessfully.");
+  if (req.userId !== user._id.toString()) {
+    return next(createError(403, "You can delete only your account!"));
+  }
+  await User.findByIdAndDelete(req.params.id);
+  next(createError(200, "Deleted Sucessfully."));
 };
