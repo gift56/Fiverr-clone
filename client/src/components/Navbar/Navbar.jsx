88f69adc41372c694205ad4dedc2c71dbb056dd0
gift56 <efeasiughu@gmail.com>
2023-03-24 10:05:50 -0700

    working on authentication

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -3,8 +3,11 @@ import { TfiWorld } from "react-icons/tfi";
 import { BsCurrencyDollar } from "react-icons/bs";
 import { Link, NavLink, useLocation } from "react-router-dom";
 import Login from "../../pages/login/Login";
+import useAuthStore from "../../stores";
+import Avatar from '../../assets/icons/avatar.jpg'
 
 const Navbar = () => {
+  const { authUser } = useAuthStore();
   const [active, setActive] = useState(false);
   const [openDrop, setOpenDrop] = useState(false);
   const { pathname } = useLocation();
@@ -19,13 +22,6 @@ const Navbar = () => {
       window.removeEventListener("scroll", backgroundChange);
     };
   }, []);
-
-  // const currentUser = {
-  //   id: 1,
-  //   username: "Efe Gift",
-  //   isSeller: true,
-  // };
-  const currentUser = false;
 
   return (
     <header
@@ -59,30 +55,33 @@ const Navbar = () => {
               </span>
               USD
             </span>
-            {!currentUser.isSeller && (
+            {!authUser.isSeller && (
               <NavLink to="/seller" className="cursor-pointer">
                 Become a Seller
               </NavLink>
             )}
-            {currentUser ? (
+            {authUser ? (
               <>
-                {currentUser && (
+                {authUser && (
                   <div
                     className="relative flex items-center gap-4 cursor-pointer"
                     onClick={() => setOpenDrop((prev) => !prev)}
                   >
                     <img
-                      src="https://cdn1.vectorstock.com/i/1000x1000/70/75/user-icon-green-vector-17597075.jpg"
+                      src={
+                        authUser.img ||
+                        Avatar
+                      }
                       alt="user_image"
                       className="w-[32px] h-[32px] rounded-[50%] object-cover"
                     />
-                    <span>{currentUser.username}</span>
+                    <span>{authUser?.username}</span>
                     <div
                       className={`absolute top-12 right-0 p-3 bg-white border rounded-md text-black flex-col items-start gap-3 w-[200px] font-medium transition-transform duration-300 ${
                         openDrop ? "flex" : "hidden"
                       }`}
                     >
-                      {currentUser?.isSeller && (
+                      {authUser?.isSeller && (
                         <>
                           <NavLink
                             to="/myGigs"
