c0c603fc4585c81df8d87416ae9b1e8513c10587
gift56 <efeasiughu@gmail.com>
2023-03-25 12:43:47 -0700

    working on gigs endpoint

--- server/controllers/gig.controller.js
+++ server/controllers/gig.controller.js
@@ -1,15 +1,28 @@
 import Gig from "../models/gig.model.js";
 import createError from "../utils/createError.js";
 
-export const createGig = (req, res, next) => {
+export const createGig = async (req, res, next) => {
+  if (!req.isSeller)
+    return next(createError(403, "Only sellers can create a gig!"));
+
+  const newGig = new Gig({
+    userId: req.userId,
+    ...req.body,
+  });
+
+  try {
+    const savedGig = await newGig.save();
+    res.status(201).json(savedGig);
+  } catch (err) {
+    next(err);
+  }
+};
+export const deleteGig = async (req, res, next) => {
   res.send("It worked");
 };
-export const deleteGig = (req, res, next) => {
+export const getGig = async (req, res, next) => {
   res.send("It worked");
 };
-export const getGig = (req, res, next) => {
-  res.send("It worked");
-};
-export const getGigs = (req, res, next) => {
+export const getGigs = async (req, res, next) => {
   res.send("It worked");
 };
