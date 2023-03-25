98c1d156dbd752aa9999dcabf55c8d8a51d8c1df
gift56 <efeasiughu@gmail.com>
2023-03-25 09:56:23 -0700

    working on registration page

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -197,6 +197,7 @@ const Register = () => {
                 placeholder="country"
                 className="bg-white  border border-[#C7CBD1] w-full h-[40px] rounded px-4 focus:border-[1.5px] focus:border-primary outline-none text-sm"
               />
+              <button type="submit" className="w-full bg-primary/80 hover:bg-primary cursor-pointer outline-none text-white rounded py-3">Register</button>
             </div>
             <div className="flex items-start justify-start flex-col gap-4 flex-1">
               <h1 className="text-2xl text-darkColor font-semibold">
