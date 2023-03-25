c5ccec1bf9d6ed035c95af8748a3bb733c35fa6f
gift56 <efeasiughu@gmail.com>
2023-03-25 13:39:55 -0700

    done with all the gigs

--- server/controllers/gig.controller.js
+++ server/controllers/gig.controller.js
@@ -39,5 +39,22 @@ export const getGig = async (req, res, next) => {
   }
 };
 export const getGigs = async (req, res, next) => {
-  res.send("It worked");
+  const q = req.query;
+  const filters = {
+    ...(q.userId && { userId: q.userId }),
+    ...(q.cat && { cat: q.cat }),
+    ...((q.min || q.max) && {
+      price: {
+        ...(q.min && { $gt: q.min }),
+        ...(q.max && { $lt: q.max }),
+      },
+    }),
+    ...(q.search && { title: { $regex: q.search, $options: "i" } }),
+  };
+  try {
+    const gigs = await Gig.find(filters).sort({ [q.sort]: -1 });
+    res.status(200).send(gigs);
+  } catch (err) {
+    next(err);
+  }
 };
