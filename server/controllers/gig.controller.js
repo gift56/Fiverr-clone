035f0aee090d6a8d4cb45a20a14c78705e1a36b9
gift56 <efeasiughu@gmail.com>
2023-03-25 12:38:57 -0700

    working on gigs endpoint

--- server/controllers/gig.controller.js
+++ server/controllers/gig.controller.js
@@ -1,12 +1,14 @@
+import createError from "../utils/createError.js";
+
 export const createGig = (req, res) => {
-    res.send("It worked")
+  res.send("It worked");
 };
 export const deleteGig = (req, res) => {
-    res.send("It worked")
+  res.send("It worked");
 };
 export const getGig = (req, res) => {
-    res.send("It worked")
+  res.send("It worked");
 };
 export const getGigs = (req, res) => {
-    res.send("It worked")
+  res.send("It worked");
 };
