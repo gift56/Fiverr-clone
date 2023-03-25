269cf09f9b6b1c47a72559b141b1965e96d8005d
gift56 <efeasiughu@gmail.com>
2023-03-25 12:09:54 -0700

    done working on registration page

--- client/src/libs/upload.js
+++ client/src/libs/upload.js
@@ -5,10 +5,7 @@ const upload = async (file) => {
   data.append("file", file);
   data.append("upload_preset", "fiverr");
   try {
-    const res = await axios.post(
-      "https://api.cloudinary.com/v1_1/efegift/image/upload",
-      data
-    );
+    const res = await axios.post(process.env.REACT_APP_CLOUDINARY_URL, data);
     const { url } = res.data;
     return url;
   } catch (error) {
