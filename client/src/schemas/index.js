586fdb1710928981eb0d1baa7a7a11c7911d12d2
gift56 <efeasiughu@gmail.com>
2023-03-25 10:26:58 -0700

    working on registration page

--- client/src/schemas/index.js
+++ client/src/schemas/index.js
@@ -13,6 +13,7 @@ export const registerSchema = yup.object({
   email: yup.string().email("Invalid Email").required("Required"),
   img: yup.string().required("Required"),
   country: yup.string().required("Required"),
+  desc: yup.string().required("Required"),
   password: yup
     .string()
     .min(5, "Password must be at least 5 characters long")
