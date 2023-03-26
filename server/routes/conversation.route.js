583c6b8fae2d7fe2acfc651f571e3062d34b5fdd
gift56 <efeasiughu@gmail.com>
2023-03-26 15:00:11 -0700

    working on conversation endpoints

--- server/routes/conversation.route.js
+++ server/routes/conversation.route.js
@@ -1,8 +1,17 @@
 import express from "express";
-import {} from "../controllers/user.controller.js";
+import {
+  createConversation,
+  getConversations,
+  getSingleConversation,
+  updateConversation,
+} from "../controllers/conversation.controller.js";
+import { verifyToken } from "../middleware/jwt.js";
 
 const router = express.Router();
 
-router.get("/test");
+router.get("/", verifyToken, getConversations);
+router.post("/", verifyToken, createConversation);
+router.get("/single/:id", verifyToken, getSingleConversation);
+router.put("/:id", verifyToken, updateConversation);
 
 export default router;
