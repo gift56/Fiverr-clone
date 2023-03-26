164a6bb51cdc857b201905f4b437f923ca61c2ea
gift56 <efeasiughu@gmail.com>
2023-03-26 16:47:38 -0700

    working on single message page

--- client/src/pages/message/Message.jsx
+++ client/src/pages/message/Message.jsx
@@ -65,36 +65,62 @@ const Message = () => {
             <span>John Doe</span> <BiChevronRight size={20} />
           </p>
           <div className="w-[80%] flex items-start justify-start gap-4 flex-col h-[500px]">
-            <div className="h-[500px] overflow-auto border p-4 rounded-md flex flex-col gap-4 w-full">
-              {mesagesData.map((item, i) => (
-                <div
-                  key={i}
-                  className={`flex items-start gap-5 max-w-[600px] ${
-                    item.owner === true ? "self-end flex-row-reverse" : ""
-                  }`}
-                >
-                  <img
-                    src="https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto/v1/attachments/generic_asset/asset/c51dbf5c160dac9bd067442911e65d16-1626181910196/Miscellaneous_2x.png"
-                    alt="sender_img"
-                    className="w-10 h-10 object-cover rounded-full border"
-                  />
-                  <p
-                    className={`text-sm p-4 rounded-2xl ${
-                      item.owner === true
-                        ? "bg-blue-500 rounded-tr-[0] text-white"
-                        : "bg-gray-200 rounded-tl-[0]"
-                    }`}
-                  >
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Perspiciatis beatae aperiam cupiditate aliquid reprehenderit
-                    voluptatem qui asperiores illo atque suscipit odio, alias
-                    doloribus repellat voluptates nesciunt distinctio! Tempora
-                    labore, voluptate hic repellat ad deserunt alias earum
-                    recusandae nulla ullam repellendus.
-                  </p>
-                </div>
-              ))}
-            </div>
+            {isLoading ? (
+              <div className="flex items-center justify-center w-full">
+                <img src={loader} alt="/" className="w-[40px]" />
+              </div>
+            ) : error ? (
+              <p className="text-2xl text-red-400 font-normal">
+                Error : Something went wrong
+              </p>
+            ) : (
+              <>
+                {data?.length === 0 ? (
+                  <div className="flex items-center justify-center mt-5 flex-col w-full">
+                    <img
+                      src="https://cdni.iconscout.com/illustration/premium/thumb/error-404-4344461-3613889.png"
+                      alt="/"
+                      className="w-[350px]"
+                    />
+                    <h2 className="text-4xl text-active font-medium">
+                      No Messages!
+                    </h2>
+                  </div>
+                ) : (
+                  <div className="h-[500px] overflow-auto border p-4 rounded-md flex flex-col gap-4 w-full">
+                    {mesagesData.map((item, i) => (
+                      <div
+                        key={i}
+                        className={`flex items-start gap-5 max-w-[600px] ${
+                          item.owner === true ? "self-end flex-row-reverse" : ""
+                        }`}
+                      >
+                        <img
+                          src="https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto/v1/attachments/generic_asset/asset/c51dbf5c160dac9bd067442911e65d16-1626181910196/Miscellaneous_2x.png"
+                          alt="sender_img"
+                          className="w-10 h-10 object-cover rounded-full border"
+                        />
+                        <p
+                          className={`text-sm p-4 rounded-2xl ${
+                            item.owner === true
+                              ? "bg-blue-500 rounded-tr-[0] text-white"
+                              : "bg-gray-200 rounded-tl-[0]"
+                          }`}
+                        >
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Perspiciatis beatae aperiam cupiditate aliquid
+                          reprehenderit voluptatem qui asperiores illo atque
+                          suscipit odio, alias doloribus repellat voluptates
+                          nesciunt distinctio! Tempora labore, voluptate hic
+                          repellat ad deserunt alias earum recusandae nulla
+                          ullam repellendus.
+                        </p>
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </>
+            )}
             <form
               onSubmit={handleSubmit}
               className="flex items-end w-full gap-8"
