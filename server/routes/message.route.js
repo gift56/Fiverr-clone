ecf4353efca615fb6dc1c60723f81db00aa90fcd
gift56 <efeasiughu@gmail.com>
2023-03-26 16:27:03 -0700

    working message endpoint

--- server/routes/message.route.js
+++ server/routes/message.route.js
@@ -1,8 +1,13 @@
 import express from "express";
-import {} from "../controllers/user.controller.js";
+import {
+  createMessage,
+  getMessages,
+} from "../controllers/message.controller.js";
+import { verifyToken } from "../middleware/jwt.js";
 
 const router = express.Router();
 
-router.get("/test");
+router.post("/", verifyToken, createMessage);
+router.get("/:id", verifyToken, getMessages);
 
 export default router;
