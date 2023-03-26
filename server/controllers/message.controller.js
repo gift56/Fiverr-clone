33945017b988bb4169eac41d07f20c6b3818b120
gift56 <efeasiughu@gmail.com>
2023-03-26 16:26:22 -0700

    working message endpoint

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -1,8 +1,37 @@
 import createError from "../utils/createError.js";
+import Message from "../models/message.model.js";
+import Conversation from "../models/conversation.model.js";
 
-// export const createOrder = async (req, res, next) => {
-//   try {
-//   } catch (err) {
-//     next(err);
-//   }
-// };
+export const createMessage = async (req, res, next) => {
+  const newMessage = new Message({
+    conversationId: req.body.conversationId,
+    userId: req.userId,
+    desc: req.body.desc,
+  });
+  try {
+    const savedMessage = await newMessage.save();
+    await Conversation.findOneAndUpdate(
+      { id: req.body.conversationId },
+      {
+        $set: {
+          readBySeller: req.isSeller,
+          readByBuyer: !req.isSeller,
+          lastMessage: req.body.desc,
+        },
+      },
+      { new: true }
+    );
+
+    res.status(201).send(savedMessage);
+  } catch (err) {
+    next(err);
+  }
+};
+export const getMessages = async (req, res, next) => {
+  try {
+    const messages = await Message.find({ conversationId: req.params.id });
+    res.status(200).send(messages);
+  } catch (err) {
+    next(err);
+  }
+};
