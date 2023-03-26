693e3a90beb654de47ce78b62b549e3852f41d2f
gift56 <efeasiughu@gmail.com>
2023-03-26 13:45:06 -0700

    working on Order endpoint

--- server/routes/order.route.js
+++ server/routes/order.route.js
@@ -1,8 +1,12 @@
 import express from "express";
-import {  } from "../controllers/user.controller.js";
+import { verifyToken } from "../middleware/jwt.js";
+import { getOrders, intent, confirm } from "../controllers/order.controller.js";
 
 const router = express.Router();
 
-router.get("/test", );
+// router.post("/:gigId", verifyToken, createOrder);
+// router.get("/", verifyToken, getOrders);
+// router.post("/create-payment-intent/:id", verifyToken, intent);
+// router.put("/", verifyToken, confirm);
 
 export default router;
