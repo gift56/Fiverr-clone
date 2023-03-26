902910d46d099347c1435644957ce91f7fc66b11
gift56 <efeasiughu@gmail.com>
2023-03-26 10:14:57 -0700

    working on reviews

--- server/routes/review.route.js
+++ server/routes/review.route.js
@@ -1,8 +1,15 @@
 import express from "express";
-import {} from "../controllers/user.controller.js";
+import { verifyToken } from "../middleware/jwt.js";
+import {
+  createReview,
+  getReviews,
+  deleteReview,
+} from "../controllers/review.controller.js";
 
 const router = express.Router();
 
-router.get("/test");
+router.post("/", verifyToken, createReview);
+router.get("/:gigId", getReviews);
+router.delete("/:id", deleteReview);
 
 export default router;
