1fea7d056eb6842094d4abea062df9c79908640a
gift56 <efeasiughu@gmail.com>
2023-03-27 12:54:07 -0700

    done with business mobile view

--- client/src/components/HomeComponents/Projects/Projects.jsx
+++ client/src/components/HomeComponents/Projects/Projects.jsx
@@ -5,7 +5,7 @@ const Projects = ({ item }) => {
   return (
     <Link
       to={`/`}
-      className="relative inline-block cursor-pointer w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] mx-4 shadow-box"
+      className="relative inline-block cursor-pointer w-[240px] lg:w-[280px] mx-4 shadow-box"
     >
       <div className="w-full flex items-start flex-col justify-start">
         <img
