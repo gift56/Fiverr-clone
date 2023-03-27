ed18cc3427d7bff427d9cbd060069a5e7bed04f3
gift56 <efeasiughu@gmail.com>
2023-03-27 13:04:36 -0700

    working mobile footer

--- client/src/components/Footer/MobileFooter/MobileFooter.jsx
+++ client/src/components/Footer/MobileFooter/MobileFooter.jsx
@@ -3,6 +3,14 @@ import { FiChevronDown } from "react-icons/fi";
 
 const MobileFooter = ({ item, i }) => {
   const [clicked, setClicked] = useState(null);
+
+  const toggle = (index) => {
+    if (clicked === index) {
+      return setClicked(null);
+    }
+    setClicked(index);
+  };
+  
   return <div className="pt-5">MobileFooter</div>;
 };
 
