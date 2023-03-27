f9fd30c6f82534a92fd60086f6dab220023bea3e
gift56 <efeasiughu@gmail.com>
2023-03-27 04:46:09 -0700

    working on mobile view

--- client/src/components/Navbar/MobileSidebar/MobileSidebar.jsx
+++ client/src/components/Navbar/MobileSidebar/MobileSidebar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const MobileSidebar = ({ show, setShow }) => {
-  return <div>MobileSidebar</div>;
+  return <div className={`flex flex-col`}>MobileSidebar</div>;
 };
 
 export default MobileSidebar;
