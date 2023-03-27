2d14227e6f37ecc02775d3d59df3c0aa1af772d6
gift56 <efeasiughu@gmail.com>
2023-03-27 13:42:48 -0700

    done with mobile footer

--- client/src/components/Footer/MobileFooter/MobileFooter.jsx
+++ client/src/components/Footer/MobileFooter/MobileFooter.jsx
@@ -18,7 +18,7 @@ const MobileFooter = () => {
         <div className="w-full rounded-lg py-2 flex flex-col gap-2">
           <div
             onClick={() => toggle(i)}
-            className="flex items-center justify-between cursor-pointer text-darkColor"
+            className="flex items-center justify-between cursor-pointer text-gray-600"
           >
             <h2 className="text-base font-semibold">{item.title}</h2>
             <span
