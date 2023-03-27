97da43237c224b0eb6ef2558232d2876b8e63c02
gift56 <efeasiughu@gmail.com>
2023-03-27 04:44:25 -0700

    working on mobile view

---
+++ client/src/components/Navbar/MobileSidebar/MobileSidebar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MobileSidebar = ({ show, setShow }) => {
+  return <div>MobileSidebar</div>;
+};
+
+export default MobileSidebar;
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -89,7 +89,7 @@ const Navbar = () => {
       }`}
     >
       <div className="contain">
-        <div className="w-full flex items-center justify-between py-4">
+        <div className="w-full flex items-center justify-between py-4 relative">
           <div className="flex items-center gap-2 h-full">
             <span className="lg:hidden mt-1">
               <FaBars size={25} />
