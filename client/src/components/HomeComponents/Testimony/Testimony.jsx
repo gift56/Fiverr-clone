46f39b27da56cb7e298d4e700db2900522afd304
gift56 <efeasiughu@gmail.com>
2023-03-27 12:50:51 -0700

    done with business mobile view

--- client/src/components/HomeComponents/Testimony/Testimony.jsx
+++ client/src/components/HomeComponents/Testimony/Testimony.jsx
@@ -5,8 +5,8 @@ import { BsPlayFill } from "react-icons/bs";
 const Testimony = ({ item }) => {
   return (
     <div className="relative inline-block cursor-pointer w-full mx-4 p-4 pt-0">
-      <div className="w-full flex items-center gap-7 justify-start">
-        <div className="w-[40%] relative">
+      <div className="w-full flex items-start lg:items-center flex-col lg:flex-row gap-7 justify-start">
+        <div className="w-full lg:w-[40%] relative">
           <img src={userImg} alt={item.title} className="w-full rounded-md" />
           <span className="absolute top-0 w-full h-full flex items-center justify-center text-white">
             <span className="bg-black/50 rounded-full flex items-center w-[60px] h-[60px] justify-center cursor-pointer">
@@ -14,9 +14,9 @@ const Testimony = ({ item }) => {
             </span>
           </span>
         </div>
-        <div className="flex flex-col items-start justify-start gap-5 w-[60%]">
-          <h2 className="text-xl tracking-wide font-semibold text-gray-500">{item.title} | </h2>
-          <p className="text-3xl max-w-[670px] whitespace-normal text-navbg">
+        <div className="flex flex-col items-start justify-start gap-5 w-full lg:w-[60%]">
+          <h2 className="sm:text-xl text-base tracking-wide font-semibold text-gray-500">{item.title} | </h2>
+          <p className="lg:text-3xl text-xl w-full lg:max-w-[670px] whitespace-normal text-navbg">
             <i>{item.details}</i>
           </p>
         </div>
