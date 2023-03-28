f1524cc6fa26d9b2a930c5b269f111e65668b176
gift56 <efeasiughu@gmail.com>
2023-03-28 15:41:06 -0700

    working on add new gigs function

--- client/src/reducers/addGigReducer.js
+++ client/src/reducers/addGigReducer.js
@@ -16,3 +16,34 @@ export const INITIAL_STATE = {
   features: [],
   price: 0,
 };
+
+export const gigReducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE_INPUT":
+      return {
+        ...state,
+        [action.payload.name]: action.payload.value,
+      };
+    case "ADD_IMAGES":
+      return {
+        ...state,
+        cover: action.payload.cover,
+        images: action.payload.images,
+      };
+    case "ADD_FEATURE":
+      return {
+        ...state,
+        features: [...state.features, action.payload],
+      };
+    case "REMOVE_FEATURE":
+      return {
+        ...state,
+        features: state.features.filter(
+          (feature) => feature !== action.payload
+        ),
+      };
+
+    default:
+      return state;
+  }
+};
