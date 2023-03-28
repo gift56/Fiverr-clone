1fdacefd0dde0e38ade1b1f32be719dbaaeb8115
gift56 <efeasiughu@gmail.com>
2023-03-28 13:34:24 -0700

    working on stripe payment process

---
+++ client/src/components/PayContents/CheckoutForm/CheckoutForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CheckoutForm = () => {
+  return (
+    <div>CheckoutForm</div>
+  )
+}
+
+export default CheckoutForm
--- client/src/pages/pay/Pay.jsx
+++ client/src/pages/pay/Pay.jsx
@@ -4,6 +4,7 @@ import { Elements } from "@stripe/react-stripe-js";
 import { useParams } from "react-router-dom";
 import { Axios } from "../../config";
 import requests from "../../libs/request";
+import CheckoutForm from "../../components/PayContents/CheckoutForm/CheckoutForm";
 
 const stripePromise = loadStripe(
   "pk_test_51LIEZEEgKFzxI7hMskyqQ92QcR3lkudfQogZ0KEup9o1bwrrGWLV41YGnM47MaM3fZwM8t7ELt4Evd5JOI5CCCv300hzK3oQrb"
@@ -25,7 +26,24 @@ const Pay = () => {
     };
     makeRequest();
   }, []);
-  return <div>Pay</div>;
+
+  const appearance = {
+    theme: "stripe",
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
+
+  return (
+    <div>
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      )}
+    </div>
+  );
 };
 
 export default Pay;
