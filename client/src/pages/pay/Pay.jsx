0e7f00097f6bc9daec09523c501ff186cfb199a4
gift56 <efeasiughu@gmail.com>
2023-03-28 13:21:14 -0700

    working on stripe payment process

--- client/src/pages/pay/Pay.jsx
+++ client/src/pages/pay/Pay.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 
+const stripePromise = loadStripe(
+  "pk_test_51LIEZEEgKFzxI7hMskyqQ92QcR3lkudfQogZ0KEup9o1bwrrGWLV41YGnM47MaM3fZwM8t7ELt4Evd5JOI5CCCv300hzK3oQrb"
+);
+
 const Pay = () => {
+  const { id } = useParams();
+  const [clientSecret, setClientSecret] = useState("");
   return <div>Pay</div>;
 };
 
