e56dbf623bd62a01ca0e1e2fecd8570dba4c3a72
gift56 <efeasiughu@gmail.com>
2023-03-28 13:40:23 -0700

    working on stripe payment process

--- client/src/components/PayContents/CheckoutForm/CheckoutForm.jsx
+++ client/src/components/PayContents/CheckoutForm/CheckoutForm.jsx
@@ -13,7 +13,38 @@ const CheckoutForm = () => {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  
+
+  useEffect(() => {
+    if (!stripe) {
+      return;
+    }
+
+    const clientSecret = new URLSearchParams(window.location.search).get(
+      "payment_intent_client_secret"
+    );
+
+    if (!clientSecret) {
+      return;
+    }
+
+    stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
+      switch (paymentIntent.status) {
+        case "succeeded":
+          setMessage("Payment succeeded!");
+          break;
+        case "processing":
+          setMessage("Your payment is processing.");
+          break;
+        case "requires_payment_method":
+          setMessage("Your payment was not successful, please try again.");
+          break;
+        default:
+          setMessage("Something went wrong.");
+          break;
+      }
+    });
+  }, [stripe]);
+
   return <div>CheckoutForm</div>;
 };
 
