8351596227e08e615a9a11cc6b3028dc89e58a08
gift56 <efeasiughu@gmail.com>
2023-03-28 14:30:07 -0700

    creadted success page

--- client/src/components/HomeComponents/Hero/Hero.jsx
+++ client/src/components/HomeComponents/Hero/Hero.jsx
@@ -6,6 +6,21 @@ import * as yup from "yup";
 import loader from "../../../assets/icons/loader.svg";
 
 const Hero = () => {
+  const [loading, setLoading] = useState(false);
+  const initialValues = {
+    input: "",
+  };
+  const validationSchema = yup.object({
+    input: yup.string().required("Required"),
+  });
+
+
+  const {} = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit,
+  });
+
   return (
     <section className="w-full bg-[#1a1b1d] lg:bg-navbg h-screen text-white lg:animate-bganimate bg-no-repeat bg-cover bg-top-4 pt-20">
       <div className="contain flex flex-col items-start justify-center h-full">
@@ -14,7 +29,7 @@ const Hero = () => {
             Find the perfect <i className="font-light">freelance</i> <br />{" "}
             services for your business
           </h2>
-          <div className="flex flex-col lg:flex-row items-center w-full gap-5 lg:w-[650px] bg-white h-[50px] rounded-md focus:border focus:border-primary relative mb-20 lg:mb-0">
+          <form className="flex flex-col lg:flex-row items-center w-full gap-5 lg:w-[650px] bg-white h-[50px] rounded-md focus:border focus:border-primary relative mb-20 lg:mb-0">
             <div className="flex items-center justify-start gap-2 w-full h-full">
               <span className="text-active pl-3">
                 <FiSearch size={18} />
@@ -31,7 +46,7 @@ const Hero = () => {
             >
               Search
             </button>
-          </div>
+          </form>
           <div className="flex items-center gap-3 flex-wrap sm:flex-row">
             <span>Popular :</span>
             {buttons.map((item, i) => (
