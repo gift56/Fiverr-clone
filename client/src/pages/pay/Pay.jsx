a59d70397d35aa6df55723513c9ae0daa1b9085a
gift56 <efeasiughu@gmail.com>
2023-03-28 13:23:15 -0700

    working on stripe payment process

--- client/src/pages/pay/Pay.jsx
+++ client/src/pages/pay/Pay.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import { useParams } from "react-router-dom";
+import { Axios } from "../../config";
+import requests from "../../libs/request";
 
 const stripePromise = loadStripe(
   "pk_test_51LIEZEEgKFzxI7hMskyqQ92QcR3lkudfQogZ0KEup9o1bwrrGWLV41YGnM47MaM3fZwM8t7ELt4Evd5JOI5CCCv300hzK3oQrb"
@@ -10,6 +12,19 @@ const stripePromise = loadStripe(
 const Pay = () => {
   const { id } = useParams();
   const [clientSecret, setClientSecret] = useState("");
+  useEffect(() => {
+    const makeRequest = async () => {
+      try {
+        const res = await Axios.post(
+          `${requests.orders}/create-payment-intent/${id}`
+        );
+        setClientSecret(res.data.clientSecret);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    makeRequest();
+  }, []);
   return <div>Pay</div>;
 };
 
