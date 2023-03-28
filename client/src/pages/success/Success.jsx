47253635fbf5569499cab34e8223bfc941bb1407
gift56 <efeasiughu@gmail.com>
2023-03-28 13:47:24 -0700

    Creadted success page

---
+++ client/src/pages/success/Success.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { Axios } from "../../config";
+import requests from "../../libs/request";
+
+const Success = () => {
+  const { search } = useLocation();
+  const navigate = useNavigate();
+  const params = new URLSearchParams(search);
+  const payment_intent = params.get("payment_intent");
+
+  useEffect(() => {
+    const makeRequest = async () => {
+      try {
+        await Axios.put(requests.orders, { payment_intent });
+        setTimeout(() => {
+          navigate("/orders");
+        }, 5000);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    makeRequest();
+  }, []);
+
+  return (
+    <div>
+      Payment successful. You are being redirected to the orders page. Please do
+      not close the page
+    </div>
+  );
+};
+
+export default Success;
