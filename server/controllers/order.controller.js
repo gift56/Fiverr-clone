950a80087ce5a98c99449432b47e586d5367d73f
gift56 <efeasiughu@gmail.com>
2023-03-28 13:05:03 -0700

    working on payment page

--- server/controllers/order.controller.js
+++ server/controllers/order.controller.js
@@ -33,25 +33,25 @@ import Gig from "../models/gig.model.js";
 //   });
 // };
 
-export const createOrder = async (req, res, next) => {
-  try {
-    const gig = await Gig.findById(req.params.gigId);
+// export const createOrder = async (req, res, next) => {
+//   try {
+//     const gig = await Gig.findById(req.params.gigId);
 
-    const newOrder = new Order({
-      gigId: gig._id,
-      img: gig.cover,
-      title: gig.title,
-      buyerId: req.userId,
-      sellerId: gig.userId,
-      price: gig.price,
-      payment_intent: "temporary",
-    });
-    await newOrder.save();
-    res.status(200).send("Successful");
-  } catch (err) {
-    next(err);
-  }
-};
+//     const newOrder = new Order({
+//       gigId: gig._id,
+//       img: gig.cover,
+//       title: gig.title,
+//       buyerId: req.userId,
+//       sellerId: gig.userId,
+//       price: gig.price,
+//       payment_intent: "temporary",
+//     });
+//     await newOrder.save();
+//     res.status(200).send("Successful");
+//   } catch (err) {
+//     next(err);
+//   }
+// };
 
 export const getOrders = async (req, res, next) => {
   try {
