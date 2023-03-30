204d66677021ed9ae5325c49605510dcde97f9e5
gift56 <efeasiughu@gmail.com>
2023-03-30 06:10:09 -0700

    adding some changes

--- server/routes/order.route.js
+++ server/routes/order.route.js
@@ -1,11 +1,6 @@
 import express from "express";
 import { verifyToken } from "../middleware/jwt.js";
-import {
-  getOrders,
-  intent,
-  confirm,
-  // createOrder,
-} from "../controllers/order.controller.js";
+import { getOrders, intent, confirm } from "../controllers/order.controller.js";
 
 const router = express.Router();
 
