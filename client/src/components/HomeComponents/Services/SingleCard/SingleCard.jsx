9bfd78abb16f16d3bae221d8dbdce4769812d979
gift56 <efeasiughu@gmail.com>
2023-03-30 07:08:42 -0700

    working fine

--- client/src/components/HomeComponents/Services/SingleCard/SingleCard.jsx
+++ client/src/components/HomeComponents/Services/SingleCard/SingleCard.jsx
@@ -4,13 +4,19 @@ import { Link } from "react-router-dom";
 const SingleCard = ({ item }) => {
   return (
     <Link
-      to={`/gigs?cat=design`}
+      to={`/gigs?cat=${item.cat}`}
       className="relative inline-block cursor-pointer w-[200px] md:w-[240px] lg:w-[250px] mx-4"
     >
-      <img src={item.img} alt={item.title} className="w-full h-auto block hover:opacity-80 transition-all duration-300 rounded-md" />
+      <img
+        src={item.cover}
+        alt={item.title}
+        className="w-full h-[320px] block hover:opacity-80 transition-all duration-300 rounded-md"
+      />
       <div className="absolute top-4 text-white left-4">
-        <p className="text-sm font-normal">{item?.description}</p>
-        <h2 className="text-2xl font-bold tracking-wide">{item.title}</h2>
+        <p className="text-sm font-normal truncate w-[200px]">{item?.desc}</p>
+        <h2 className="text-2xl font-bold tracking-wide truncate w-[200px]">
+          {item.title}
+        </h2>
       </div>
     </Link>
   );
--- client/src/pages/homepage/Homepage.jsx
+++ client/src/pages/homepage/Homepage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Clients from "../../components/HomeComponents/Clients/Clients";
 import Hero from "../../components/HomeComponents/Hero/Hero";
 import Slides from "../../components/Slides/Slides";
@@ -9,17 +9,57 @@ import Marketplace from "../../components/HomeComponents/Marketplace/Marketplace
 import Business from "../../components/HomeComponents/Business/Business";
 import Testimony from "../../components/HomeComponents/Testimony/Testimony";
 import Projects from "../../components/HomeComponents/Projects/Projects";
+import { useQuery } from "@tanstack/react-query";
+import { Axios } from "../../config";
+import requests from "../../libs/request";
+import loader from "../../assets/icons/loader.svg";
 
 const Homepage = () => {
+  const { isLoading, error, data, refetch } = useQuery({
+    queryKey: ["gigs"],
+    queryFn: () => Axios.get(requests.gigs).then((res) => res.data),
+  });
+
+  console.log(data);
+
   return (
     <main>
       <Hero />
       <Clients />
-      <Slides rowId="1" distance={500} text="Popular professional services">
-        {cards.map((item) => (
-          <SingleCard key={item.id} item={item} />
-        ))}
-      </Slides>
+      {isLoading ? (
+        <div className="flex items-center justify-center w-full">
+          <img src={loader} alt="/" className="w-[40px]" />
+        </div>
+      ) : error ? (
+        <p className="text-2xl text-red-400 font-normal">
+          Error : Something went wrong
+        </p>
+      ) : (
+        <>
+          {data?.length === 0 ? (
+            <div className="flex items-center justify-center mt-5 flex-col w-full">
+              <img
+                src="https://cdni.iconscout.com/illustration/premium/thumb/error-404-4344461-3613889.png"
+                alt="/"
+                className="w-[350px]"
+              />
+              <h2 className="text-2xl md:text-4xl text-active font-medium text-center">
+                Oops!🤷‍♂️ No Result
+              </h2>
+            </div>
+          ) : (
+            <Slides
+              rowId="1"
+              distance={500}
+              text="Popular professional services"
+            >
+              {data.map((item) => (
+                <SingleCard key={item._id} item={item} />
+              ))}
+            </Slides>
+          )}
+        </>
+      )}
       <Features />
       <Marketplace />
       <Business />
