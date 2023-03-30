1404115caea6239f609f8d3fe1df6c4faa81b531
gift56 <efeasiughu@gmail.com>
2023-03-30 16:41:45 -0700

    it"s working fine an I deployed my backed sserver for free

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const Axios = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://careful-bull-pumps.cyclic.app",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
