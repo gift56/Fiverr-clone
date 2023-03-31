741b9689de15faf7071c7461f10e775fab9f0180
gift56 <efeasiughu@gmail.com>
2023-03-30 17:16:53 -0700

    it"s working fine an I deployed my backed sserver for free

--- server/server.js
+++ server/server.js
@@ -33,6 +33,16 @@ app.use(
     credentials: true,
   })
 );
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://fiverr-clone-nine.vercel.app"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoute);
