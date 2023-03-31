391773db38b261db0734f6da3d7bf5f01445c2e3
gift56 <efeasiughu@gmail.com>
2023-03-30 17:13:02 -0700

    it"s working fine an I deployed my backed sserver for free

--- server/server.js
+++ server/server.js
@@ -27,7 +27,12 @@ const connectMongodb = async () => {
   }
 };
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://fiverr-clone-nine.vercel.app/"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoute);
